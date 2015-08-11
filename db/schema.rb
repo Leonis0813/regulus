5a4df20dda3e46da2cac3221a0db9b03b4f3fa74
fx <fx@tk2-209-14108.vs.sakura.ne.jp>
2015-08-11 20:31:32 +0900

    create infomation models

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809005341) do
+ActiveRecord::Schema.define(version: 20150811112759) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "content",    limit: 255
+    t.date     "date"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "currencies", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.integer  "price",      limit: 4
+    t.date     "date"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "content",    limit: 255
     t.date     "date"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
