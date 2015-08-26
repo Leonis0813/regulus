a09707b866df485bcc39f619de176385677239a8
Leonis <fx@tk2-209-14108.vs.sakura.ne.jp>
2015-08-26 21:49:42 +0900

    modify get_currencies

--- app/controllers/confirmation_controller.rb
+++ app/controllers/confirmation_controller.rb
@@ -1,82 +1,17 @@
 class ConfirmationController < ApplicationController
   def show
-    currencies = Currency.get_currencies('USDJPY')
-    @currencies_usd = currencies.map.with_index do |cur, i|
-      next if i > 19
-      [
-       cur.time.strftime('%H:%M:%S'),
-       cur.open,
-       cur.high,
-       cur.low,
-       currencies[i+1].open,
-      ]
-    end.compact
-
-    currencies = Currency.get_currencies('EURJPY')
-    @currencies_eur = currencies.map.with_index do |cur, i|
-      next if i > 19
-      [
-       cur.time.strftime('%H:%M:%S'),
-       cur.open,
-       cur.high,
-       cur.low,
-       currencies[i+1].open,
-      ]
-    end.compact
-
-    currencies = Currency.get_currencies('GBPJPY')
-    @currencies_gbp = currencies.map.with_index do |cur, i|
-      next if i > 19
-      [
-       cur.time.strftime('%H:%M:%S'),
-       cur.open,
-       cur.high,
-       cur.low,
-       currencies[i+1].open,
-      ]
-    end.compact
-
+    @currencies_usd = Currency.get_currencies('USDJPY', 5)
+    @currencies_eur = Currency.get_currencies('EURJPY', 5)
+    @currencies_gbp = Currency.get_currencies('GBPJPY', 5)
     @tweets = Tweet.get_tweets
     @articles = Article.get_articles
     render :status => :ok
   end
 
   def update_currency
-    currencies = Currency.get_currencies('USDJPY')
-    @currencies_usd = currencies.map.with_index do |cur, i|
-      next if i > 19
-      [
-       cur.time.strftime('%H:%M:%S'),
-       cur.open,
-       cur.high,
-       cur.low,
-       currencies[i+1].open,
-      ]
-    end.compact
-
-    currencies = Currency.get_currencies('EURJPY')
-    @currencies_eur = currencies.map.with_index do |cur, i|
-      next if i > 19
-      [
-       cur.time.strftime('%H:%M:%S'),
-       cur.open,
-       cur.high,
-       cur.low,
-       currencies[i+1].open,
-      ]
-    end.compact
-
-    currencies = Currency.get_currencies('GBPJPY')
-    @currencies_gbp = currencies.map.with_index do |cur, i|
-      next if i > 19
-      [
-       cur.time.strftime('%H:%M:%S'),
-       cur.open,
-       cur.high,
-       cur.low,
-       currencies[i+1].open,
-      ]
-    end.compact
+    @currencies_usd = Currency.get_currencies('USDJPY', 5)
+    @currencies_eur = Currency.get_currencies('EURJPY', 5)
+    @currencies_gbp = Currency.get_currencies('GBPJPY', 5)
   end
 
   def update_tweet
