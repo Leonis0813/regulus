2474910d3910e903d0f2a30f064c4ce6f56052e3
Leonis <fx@tk2-209-14108.vs.sakura.ne.jp>
2015-09-02 14:16:49 +0900

    create tweet test

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Tweet::get_tweets' do
+    assert_equal 20, Tweet.get_tweets.size
+  end
 end
