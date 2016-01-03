995020a2cee4db76eb957e4f0f9721780922ef05
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-01-03 21:13:51 +0900

    create currencies_controller_spec.rb

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -3,5 +3,6 @@ class CurrenciesController < ApplicationController
     @currencies_usd = Currency.get_currencies('USDJPY', 5)
     @currencies_eur = Currency.get_currencies('EURJPY', 5)
     @currencies_gbp = Currency.get_currencies('GBPJPY', 5)
+    render :nothing => true
   end
 end
