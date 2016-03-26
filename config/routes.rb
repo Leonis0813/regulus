4d35205765beb53537153a584775d8bfb3d226d5
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-03-26 21:01:48 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'confirmation#show'
-  get '/rate' => 'ratess#update'
-  get '/tweet' => 'tweets#update'
-  get '/article' => 'articles#update'
+  get '/rate' => 'rates#show'
+  get '/tweet' => 'tweets#show'
+  get '/article' => 'articles#show'
+  get '/rate/update' => 'rates#update'
+  get '/tweet/update' => 'tweets#update'
+  get '/article/update' => 'articles#update'
 end
