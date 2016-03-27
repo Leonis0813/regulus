6d5b6c0643bede83ca1cccdf63558ef2c3a5d1e5
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-03-27 15:02:25 +0900

    refactor model test for rates

--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -1,6 +1,7 @@
+# coding: utf-8
 require 'rails_helper'
 
-shared_context 'create rates' do |num_rate|
+shared_context 'レートを作成する' do |num_rate|
   before(:all) do
     num_rate.times do |i|
       rate = Rate.new
@@ -19,44 +20,41 @@ shared_context 'create rates' do |num_rate|
   after(:all) { Rate.delete_all }
 end
 
+shared_context 'レートを取得する' do |params = {}|
+  before(:all) { @res = Rate.get_rates(params[:pair], params[:interval]) }
+end
+
+shared_examples 'レートが取得されていること' do |expected_size|
+  it { expect(@res.size).to eq(expected_size) }
+end
+
+shared_examples 'レートが取得されていないこと' do
+  it { expect(@res).to be_nil }
+end
+
 describe Rate, :type => :model do
-  context 'more than 30 rates in database' do
-    include_context 'create rates', 50
-
-    it 'should return 30 rates' do
-      expect(Rate.get_rates('USDJPY', 5).size).to eq(30)
+  [
+    ['レート情報が十分にある場合', 50, 30],
+    ['レート情報は不十分な場合', 5, 5],
+  ].each do |description, num_rate, expected_size|
+    context description do
+      include_context 'レートを作成する', num_rate
+      include_context 'レートを取得する', {:pair => 'USDJPY', :interval => 5}
+      it_behaves_like 'レートが取得されていること', expected_size
     end
   end
 
-  context 'less than 30 rates in database' do
-    include_context 'create rates', 5
-
-    it 'should return 5 rates' do
-      expect(Rate.get_rates('USDJPY', 5).size).to eq(5)
+  [
+    ['為替ペアコードを指定しない場合', {:interval => 5}],
+    ['期間を指定しない場合', {:pair => 'USDJPY'}],
+    ['為替ペアコードと期間を指定しない場合', {}],
+    ['為替ペアコードが不正な場合', {:pair => 'INVALID', :interval => 5}],
+    ['期間が不正な場合', {:pair => 'USDJPY', :interval => -1}],
+    ['為替ペアコードと期間が不正な場合', {:pair => 'INVALID', :interval => -1}],
+  ].each do |description, params|
+    context description do
+      include_context 'レートを取得する', params
+      it_behaves_like 'レートが取得されていないこと'
     end
-  end
-
-  it 'should return nil if pair is nil' do
-    expect(Rate.get_rates(nil, 5)).to eq(nil)
-  end
-
-  it 'should return nil if interval is nil' do
-    expect(Rate.get_rates('USDJPY', nil)).to eq(nil)
-  end
-
-  it 'should return nil if pair and interval are nil' do
-    expect(Rate.get_rates(nil, nil)).to eq(nil)
-  end
-
-  it 'should return nil if invalid pair' do
-    expect(Rate.get_rates('INVALID', 5)).to eq(nil)
-  end
-
-  it 'should return nil if invalid interval' do
-    expect(Rate.get_rates('USDJPY', -1)).to eq(nil)
-  end
-
-  it 'should return nil if invalid pair and interval' do
-    expect(Rate.get_rates('INVALID', -1)).to eq(nil)
   end
 end
