96130de3388613dbad2e3e786d9b185dde5b61c0
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-04-03 20:03:49 +0900

    update scripts for rates

--- script/rates.dump.rb
+++ script/rates.dump.rb
@@ -17,9 +17,9 @@ ORDER BY
 EOF
 rates = `mysql --user=root --password=7QiSlC?4 regulus -e "#{query}"`
 
-backup_dir = "backup/rates/#{yesterday.strftime('%Y-%m')}"
+backup_dir = File.join(File.dirname(File.absolute_path(__FILE__)), "backup/rates/#{yesterday.strftime('%Y-%m')}")
 FileUtils.mkdir_p backup_dir unless File.exists? backup_dir
-csv_file = "#{backup_dir}/#{yesterday.strftime('%d')}.csv"
+csv_file = File.join(backup_dir, "#{yesterday.strftime('%d')}.csv")
 File.open(csv_file, 'w') do |out|
   rates.split("\n").each {|rate| out.puts(rate.tr("\t", ',')) }
 end
