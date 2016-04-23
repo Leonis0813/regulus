482b6dcd9e83c82b18960cad42237489a62cf25d
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-04-23 15:49:43 +0900

    replace jqplot with d3

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -7,7 +7,7 @@ class Rate < ActiveRecord::Base
     return nil unless interval && interval > 0
 
     [].tap do |arr|
-      Rate.pair(pair).interval("#{interval}-min").order('to_date DESC').select('to_date, open, close, high, low').limit(30).each do |record|
+      Rate.pair(pair).interval("#{interval}-min").order('to_date DESC').select('to_date, open, close, high, low').limit(100).each do |record|
         arr << [(record.to_date + 1).strftime('%Y-%m-%d %H:%M:%S'), record.open, record.high, record.low, record.close]
       end
     end
