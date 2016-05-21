d9b3e9dc990d30161ce3998456aa0c2028f63122
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-05-21 21:54:50 +0900

    use capybara on request spec

--- spec/requests/confirm_fx_info_spec.rb
+++ spec/requests/confirm_fx_info_spec.rb
@@ -2,55 +2,57 @@
 require 'rails_helper'
 
 describe '通貨情報を確認する', :type => :request do
-  shared_examples 'アクセスできること' do
-    it { expect(@res.code).to eq(200) }
+  shared_examples 'リンクの状態が正しいこと' do |selected_link|
+    [['レート', '/rates'], ['ツイート', '/tweets'], ['記事', '/articles']].each do |text, path|
+      it "'#{text}'へのリンクが表示されていること" do
+        condition = path.match(/#{selected_link}/) ? 'selected' : 'not-selected'
+        expect(page).to have_selector("a[href='#{path}'][class='#{condition}']", :text => text)
+      end
+    end
   end
 
-  shared_examples 'タブが表示されていること' do
-    it { expect(@res.body).to match /a.*href="\/rates"/ }
-    it { expect(@res.body).to match /a.*href="\/tweets"/ }
-    it { expect(@res.body).to match /a.*href="\/articles"/ }
+  shared_examples 'セレクトボックスの状態が正しいこと' do |pair, interval|
+    [pair, interval].each do |selected|
+      it { expect(page).to have_xpath("//form/select/option[text()='#{selected}'][@selected]") }
+    end
   end
 
-  shared_examples '表示されているデータが正しいこと' do |resource|
-    it { expect(@res.body).to match /div.*id="#{resource}"/ }
-    it { expect(@res.body).to match /a.*href="\/#{resource}s".*class="selected"/ }
+  shared_examples '表示されているデータが正しいこと' do |content, id|
+    it "#{content}が表示されていること" do
+      expect(page).to have_selector("div##{id}")
+    end
   end
-
-  include_context '共通設定'
 
   describe 'ルートパスにアクセスする' do
-    before(:all) { @res = @hc.get("#{@base_url}/") }
-
-    it 'レート画面にリダイレクトされること' do
-      expect(@res.headers['Location']).to eq("#{@base_url}/rates")
+    before(:all) do
+      page.driver.browser.authorize('dev', '.dev')
+      visit '/'
     end
 
-    describe 'リダイレクト先へアクセスする' do
-      before(:all) do
-        encoded_key = Base64::encode64("dev:.dev")
-        @hc.set_auth(@res.headers['Location'], 'dev', '.dev');
-        @res = @hc.get(@res.headers['Location'])
+    it 'レート画面にリダイレクトされていること' do
+      expect(current_url).to eq("#{Capybara.app_host}/rates")
+    end
+
+    it_behaves_like 'リンクの状態が正しいこと', 'rate'
+    it_behaves_like 'セレクトボックスの状態が正しいこと', 'USDJPY', '5-min'
+    it_behaves_like '表示されているデータが正しいこと', 'レート', 'rate'
+
+    describe 'ツイートを表示する' do
+      before(:all) { click_link 'ツイート' }
+      it_behaves_like 'リンクの状態が正しいこと', 'tweet'
+      it_behaves_like '表示されているデータが正しいこと', 'ツイート', 'tweet'
+
+      describe '記事を表示する' do
+        before(:all) { click_link '記事' }
+        it_behaves_like 'リンクの状態が正しいこと', 'article'
+        it_behaves_like '表示されているデータが正しいこと', '記事', 'article'
       end
 
-      it_behaves_like 'アクセスできること'
-      it_behaves_like 'タブが表示されていること'
-      it_behaves_like '表示されているデータが正しいこと', 'rate'
-
-      describe 'ツイートを表示する' do
-        before(:all) { @res = @hc.get("#{@base_url}/tweets") }
-
-        it_behaves_like 'アクセスできること'
-        it_behaves_like 'タブが表示されていること'
-        it_behaves_like '表示されているデータが正しいこと', 'tweet'
-
-        describe '記事を表示する' do
-          before(:all) { @res = @hc.get("#{@base_url}/articles") }
-
-          it_behaves_like 'アクセスできること'
-          it_behaves_like 'タブが表示されていること'
-          it_behaves_like '表示されているデータが正しいこと', 'article'
-        end
+      describe 'レートを表示する' do
+        before(:all) { click_link 'レート' }
+        it_behaves_like 'リンクの状態が正しいこと', 'rate'
+        it_behaves_like 'セレクトボックスの状態が正しいこと', 'USDJPY', '5-min'
+        it_behaves_like '表示されているデータが正しいこと', 'レート', 'rate'
       end
     end
   end
--- spec/support/requests/context/common.rb
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-shared_context '共通設定' do
-  before(:all) do
-    @base_url = 'http://160.16.66.112:888'
-    @content_type_json = {'Content-Type' => 'application/json'}
-    @hc = HTTPClient.new
-  end
-end
