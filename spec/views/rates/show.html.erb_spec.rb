322be2773a7f18025382f964c6ea5c36e28b1a71
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-05-22 01:38:51 +0900

    update view spec

--- spec/views/rates/show.html.erb_spec.rb
+++ spec/views/rates/show.html.erb_spec.rb
@@ -2,9 +2,36 @@
 require 'rails_helper'
 
 describe "rates/show", :type => :view do
-  before { render :template => 'rates/show.html.erb' }
+  before(:all) { @response = nil }
+
+  before(:each) do
+    render
+    @response ||= response
+  end
 
   it 'div タグが表示されていること' do
-    expect(response).to match /div.*id="rate"/
+    expect(@response).to have_selector('div#rate')
+  end
+
+  it '<form>タグがあること' do
+    expect(@response).to have_selector('form[action="/rates"][method="get"]')
+  end
+
+  %w[ pair interval ].each do |id|
+    it "id=#{id}の<select>タグがあること" do
+      expect(@response).to have_selector("select##{id}")
+    end
+  end
+
+  (Settings.pairs + Settings.intervals).each do |text|
+    it "#{text}が選択できること" do
+      expect(@response).to have_selector('option', :text => text)
+    end
+  end
+
+  %w[ USDJPY 5-min ].each do |text|
+    it "#{text}が選択されていること" do
+      expect(@response).to have_selector('option[selected]', :text => text)
+    end
   end
 end
