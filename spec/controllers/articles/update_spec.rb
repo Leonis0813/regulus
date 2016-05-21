f75368acd4cc0fce1c85d2cad58287a598ecc0ed
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-05-21 17:57:27 +0900

    divide spec for article

--- spec/controllers/articles/update_spec.rb
+++ spec/controllers/articles/update_spec.rb
@@ -20,22 +20,10 @@ describe ArticlesController, :type => :controller do
 
   include_context '記事を作成する', 20
   include_context 'ユーザー名とパスワードをセットする'
-
-  describe 'GET #show' do
-    include_context 'インスタンス変数初期化'
-    before(:each) do
-      @res ||= get(:show)
-      @articles ||= assigns[:articles]
-    end
-    it_behaves_like 'ステータスコードとインスタンス変数が正しいこと'
+  include_context 'インスタンス変数初期化'
+  before(:each) do
+    @res ||= xhr(:get, :update)
+    @articles ||= assigns[:articles]
   end
-
-  describe 'GET #update' do
-    include_context 'インスタンス変数初期化'
-    before(:each) do
-      @res ||= xhr(:get, :update)
-      @articles ||= assigns[:articles]
-    end
-    it_behaves_like 'ステータスコードとインスタンス変数が正しいこと'
-  end
+  it_behaves_like 'ステータスコードとインスタンス変数が正しいこと'
 end
