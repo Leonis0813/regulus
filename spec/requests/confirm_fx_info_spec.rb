635a512d947f819ba6e727de06017c3e1b49e6b7
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-05-22 13:01:42 +0900

    fix bug for requests spec

--- spec/requests/confirm_fx_info_spec.rb
+++ spec/requests/confirm_fx_info_spec.rb
@@ -1,28 +1,28 @@
 # coding: utf-8
 require 'rails_helper'
 
-describe '通貨情報を確認する', :type => :request do
+describe '通貨情報を確認する', :type => :request, :js => true do
   describe 'ルートパスにアクセスする' do
     include_context '認証情報を入力する'
-    before(:all) { visit '/' }
+    before(:each) { visit '/' }
     it_behaves_like 'レート画面にリダイレクトされていること'
     it_behaves_like 'リンクの状態が正しいこと', 'rate'
     it_behaves_like 'セレクトボックスの状態が正しいこと', 'USDJPY', '5-min'
     it_behaves_like '表示されているデータが正しいこと', 'レート', 'rate'
 
     describe 'ツイートを表示する' do
-      before(:all) { click_link 'ツイート' }
+      before(:each) { click_link 'ツイート' }
       it_behaves_like 'リンクの状態が正しいこと', 'tweet'
       it_behaves_like '表示されているデータが正しいこと', 'ツイート', 'tweet'
 
       describe '記事を表示する' do
-        before(:all) { click_link '記事' }
+        before(:each) { click_link '記事' }
         it_behaves_like 'リンクの状態が正しいこと', 'article'
         it_behaves_like '表示されているデータが正しいこと', '記事', 'article'
       end
 
       describe 'レートを表示する' do
-        before(:all) { click_link 'レート' }
+        before(:each) { click_link 'レート' }
         it_behaves_like 'リンクの状態が正しいこと', 'rate'
         it_behaves_like 'セレクトボックスの状態が正しいこと', 'USDJPY', '5-min'
         it_behaves_like '表示されているデータが正しいこと', 'レート', 'rate'
