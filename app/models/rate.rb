ac93221d6203b239af4f9774fc5ab5e2d9e31ec8
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-07-02 17:03:45 +0900

    update model

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -2,19 +2,36 @@ class Rate < ActiveRecord::Base
   scope :interval, ->(interval) { where(:interval => interval) }
   scope :pair, ->(pair) { where(:pair => pair) }
 
-  def self.get_rates(pair, interval)
-    return nil if Rate.pair(pair).size == 0
-    return nil unless interval
+  class << self
+    def get_rates(pair, interval)
+      return nil if Rate.pair(pair).size == 0
+      return nil unless interval
 
-    [].tap do |arr|
-      Rate.pair(pair).interval(interval).order('to_date DESC').select('to_date, open, close, high, low').limit(100).each do |record|
-        arr << {
-          :time => (record.to_date + 1).strftime('%Y-%m-%d %H:%M:%S'),
-          :open => record.open,
-          :high => record.high,
-          :low => record.low,
-          :close => record.close,
-        }
+      [].tap do |arr|
+        Rate.pair(pair).interval(interval).order('to_date DESC').select('to_date, open, close, high, low').limit(100).each do |record|
+          arr << {
+            :time => (record.to_date + 1).strftime('%Y-%m-%d %H:%M:%S'),
+            :open => record.open,
+            :high => record.high,
+            :low => record.low,
+            :close => record.close,
+          }
+        end
+      end
+    end
+
+    def get_moving_average(pair, interval)
+      return nil if Rate.pair(pair).size == 0
+      return nil unless interval
+
+      [].tap do |arr|
+        open_rates = Rate.pair(pair).interval(interval).order('to_date DESC').select('to_date, open').limit(174)
+        100.times do |i|
+          arr << {
+            :time => (open_rates[i].to_date + 1).strftime('%Y-%m-%d %H:%M:%S'),
+            :average => open_rates[(-i-75)..(-i-1)].map(&:open).inject(:+)/75,
+          }
+        end
       end
     end
   end
