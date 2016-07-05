5003df0f97431fd4d5960bcf58579ba56e89ba2a
Leonis <Leonis.0813@gmail.com>
2016-07-05 22:36:27 +0900

    fix bug (#39)
    * reverse moving average
    
    * modify time zone

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -10,7 +10,7 @@ class Rate < ActiveRecord::Base
       [].tap do |arr|
         Rate.pair(pair).interval(interval).order('to_date DESC').select('to_date, open, close, high, low').limit(100).each do |record|
           arr << {
-            :time => (record.to_date + 1).strftime('%Y-%m-%d %H:%M:%S'),
+            :time => (record.to_date + 1 + 9 * 60 * 60).strftime('%Y-%m-%d %H:%M:%S'),
             :open => record.open,
             :high => record.high,
             :low => record.low,
@@ -28,8 +28,8 @@ class Rate < ActiveRecord::Base
         open_rates = Rate.pair(pair).interval(interval).order('to_date DESC').select('to_date, open').limit(174)
         (open_rates.size - 74) < 0 ? [] : (open_rates.size - 74).times do |i|
           arr << {
-            :time => (open_rates[i].to_date + 1).strftime('%Y-%m-%d %H:%M:%S'),
-            :average => open_rates[(-i-75)..(-i-1)].map(&:open).inject(:+)/75,
+            :time => (open_rates[i].to_date + 1 + 9 * 60 * 60).strftime('%Y-%m-%d %H:%M:%S'),
+            :average => open_rates[i..i+74].map(&:open).inject(:+)/75,
           }
         end
       end
