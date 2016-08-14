503cd8b3dde5be1e467110396de2d3eb4f15f792
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-08-14 21:38:12 +0900

    rate.import use mysql client

--- script/rates.import.rb
+++ script/rates.import.rb
@@ -1,7 +1,7 @@
 require 'json'
-require 'mysql2'
 require 'net/http'
 require_relative 'config/settings'
+require_relative 'lib/mysql_client'
 
 IMPORT = Settings.rate['import']
 ENV['TZ'] = 'UTC'
@@ -17,36 +17,28 @@ end
 def get_rates
   now = Time.now.strftime('%Y-%m-%d %H:%M:%S')
 
-  IMPORT['pairs'].each do |pair_code|
-    parsed_url = URI.parse("#{IMPORT['url']}/?code=#{pair_code}=FX")
+  IMPORT['pairs'].each do |pair|
+    parsed_url = URI.parse("#{IMPORT['url']}/?code=#{pair}=FX")
     req = Net::HTTP::Get.new(parsed_url)
     res = Net::HTTP.start(parsed_url.host, parsed_url.port) {|http| http.request req }
 
-    rates = res.body.scan(/(.*#{pair_code}_detail_[bid|ask].*)/).flatten
-    bid = rates.find {|rate| rate.include?('bid') }.gsub(/<.*?>/, '')
-    ask = rates.find {|rate| rate.include?('ask') }.gsub(/<.*?>/, '')
+    rates = res.body.scan(/(.*#{pair}_detail_[bid|ask].*)/).flatten
+    bid = rates.find {|rate| rate.include?('bid') }.gsub(/<.*?>/, '').to_f
+    ask = rates.find {|rate| rate.include?('ask') }.gsub(/<.*?>/, '').to_f
 
     unless res.code == '200'
       log "{status: #{res.code}, message: #{res.message}, uri: #{res.uri.to_s}, error_type: #{res.error_type}}"
       next
     end
 
-    if bid.to_f == 0.0 or ask.to_f == 0.0
-      log "{status: #{res.code}, uri: #{res.uri.to_s}, pair: #{pair_code}, bid: #{bid}, ask: #{ask}}"
+    if bid == 0.0 or ask == 0.0
+      log "{status: #{res.code}, uri: #{res.uri.to_s}, pair: #{pair}, bid: #{bid}, ask: #{ask}}"
       redo
     end
 
-    query = <<"EOF"
-INSERT INTO
-  rates
-VALUES (
-  '#{now}', '#{pair_code}', #{bid.to_f}, #{ask.to_f}
-)
-EOF
-    client = Mysql2::Client.new(Settings.mysql)
-    client.query(query)
-    client.close
-    log "{status: #{res.code}, uri: #{res.uri.to_s}, pair: #{pair_code}, bid: #{bid}, ask: #{ask}}"
+    param = {:time => now, :pair => pair, :bid => bid, :ask => ask}
+    execute_sql('regulus', __FILE__.sub('.rb', '.sql'), param)
+    log "{status: #{res.code}, uri: #{res.uri.to_s}, pair: #{pair}, bid: #{bid}, ask: #{ask}}"
   end
 end
 
