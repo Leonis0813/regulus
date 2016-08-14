34f02917a6dc05337813fa4ae66d4f40fcd7ecfe
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-08-14 17:37:04 +0900

    remove constants from tweet.import

--- script/tweets.import.rb
+++ script/tweets.import.rb
@@ -1,21 +1,22 @@
-# -*- coding: utf-8 -*-
 require 'mysql2'
 require 'twitter'
+require_relative 'config/settings'
 
-CLIENT = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'qb5wB14dXWbl5nTz7n7uWUiaE'
-  config.consumer_secret = 'zU9FOSNDlQccN0gkkmD9iiHpjYBVuIsLQ9xDFGWSokFP2o4hSc'
-  config.access_token = '3313362716-oecmOzhHQnwI7IrIb2WRRtq6LLRiVlrJpSV71oN'
-  config.access_token_secret = 't1n8iXnbfCiZKiwJu0RUtHjoiCGw1fFL50pqNyPlZ2Cfw'
-end
-
+IMPORT = Settings.tweet['import']
 ENV['TZ'] = 'UTC'
+CLIENT = Twitter::REST::Client.new do |config|
+  config.consumer_key = IMPORT['consumer_key']
+  config.consumer_secret = IMPORT['consumer_secret']
+  config.access_token = IMPORT['access_token']
+  config.access_token_secret = IMPORT['access_token_secret']
+end
 
 def get_tweets
   now = Time.now.strftime('%Y-%m-%d %H:%M:%S')
 
-  tweets = CLIENT.search('為替', :count => 20, :result_type => 'recent')
-  tweets.take(100).each do |tweet|
+  query = IMPORT['query']
+  tweets = CLIENT.search(query['word'], :count => query['count'], :result_type => query['result_type'])
+  tweets.take(query['max_count']).each do |tweet|
     query = <<"EOF"
 INSERT INTO
   tweets
@@ -27,9 +28,9 @@ ON DUPLICATE KEY UPDATE
   profile_image_url = VALUES(profile_image_url),
   full_text = VALUES(full_text)
 EOF
-    %w[regulus regulus_development regulus_production].each do |db|
+    IMPORT['databases'].each do |db|
       begin
-        client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "7QiSlC?4", :database => db)
+        client = Mysql2::Client.new(Settings.mysql.merge('database' => db))
         client.query(query)
         client.close
         puts [
