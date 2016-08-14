e9b214cb0c4e0d5b26b47c40a0e2c8447da718df
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-08-14 20:11:20 +0900

    article.import use mysql client

--- script/articles.import.rb
+++ script/articles.import.rb
@@ -1,36 +1,25 @@
 require 'feedjira'
 require 'mysql2'
 require_relative 'config/settings'
+require_relative 'lib/mysql_client'
 
 IMPORT = Settings.article['import']
 ENV['TZ'] = 'UTC'
 
-feed = Feedjira::Feed.fetch_and_parse IMPORT['url']
 now = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+feed = Feedjira::Feed.fetch_and_parse IMPORT['url']
 feed.entries.each do |entry|
-  query = <<"EOF"
-INSERT INTO
-  articles
-VALUES (
-  DATE_FORMAT('#{entry.published}', '%Y-%m-%d %H:%i:%S'), "#{entry.title}", "#{entry.summary}", '#{entry.url}', '#{now}'
-)
-ON DUPLICATE KEY UPDATE
-  summary = VALUES(summary),
-  url = VALUES(url)
-EOF
+  params = %i[ published title summary url ].map {|key| [key, entry.send(key)] }.to_h
+
   IMPORT['databases'].each do |db|
     begin
-      client = Mysql2::Client.new(Settings.mysql.merge('database' => db))
-      client.query(query)
+      execute_sql(db, __FILE__.sub('.rb', '.sql'),   params.merge(:created_at => now))
       puts [
         "[#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}]",
         '[import]',
         "{database: #{db}, url: #{entry.url}}",
       ].join(' ')
     rescue
-      next
-    ensure
-      client.close
     end
   end
 end
