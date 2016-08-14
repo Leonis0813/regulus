625e89235e1abad3ddbd71363d55b9ce1512aa76
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-08-14 20:27:34 +0900

    tweet.dump use mysql client

--- script/tweets.dump.rb
+++ script/tweets.dump.rb
@@ -1,38 +1,25 @@
 require 'date'
 require 'fileutils'
-require 'mysql2'
 require_relative 'config/settings'
+require_relative 'lib/mysql_client'
 
 DUMP = Settings.tweet['dump']
 ENV['TZ'] = 'UTC'
 
-today = ARGV[0] ? Date.parse(ARGV[0]) : Date.today
-yesterday = today - 1
-
-from = yesterday.strftime('%Y-%m-%d 00:00:00')
-to = yesterday.strftime('%Y-%m-%d 23:59:59')
-query = <<"EOF"
-SELECT
-  *
-FROM
-  tweets
-WHERE
-  created_at BETWEEN '#{from}' AND '#{to}'
-ORDER BY
-  created_at
-EOF
-
+yesterday = (ARGV[0] ? Date.parse(ARGV[0]) : Date.today) - 1
 backup_dir = File.join(DUMP['backup_dir'], yesterday.strftime('%Y-%m'))
 FileUtils.mkdir_p backup_dir
 csv_file = File.join(backup_dir, "#{yesterday.strftime('%d')}.csv")
 
 File.open(csv_file, 'w') do |out|
   out.puts(DUMP['header'])
-  client = Mysql2::Client.new(Settings.mysql)
-  client.query(query).each do |tweet|
-    out.puts(tweet.values.join(','))
-  end
-  client.close
+
+  param = {
+    :from = yesterday.strftime('%Y-%m-%d 00:00:00'),
+    :to = yesterday.strftime('%Y-%m-%d 23:59:59'),
+  }
+  tweets = execute_sql('regulus', __FILE__.sub('.rb', '.sql'), param)
+  tweets.each {|tweet| out.puts(tweet.values.join(',')) }
 end
 
 puts [
