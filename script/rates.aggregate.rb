c241522183eef485d08c043228e39489f50c37f1
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-08-14 17:54:53 +0900

    remove constants from rate.aggregate

--- script/rates.aggregate.rb
+++ script/rates.aggregate.rb
@@ -1,16 +1,13 @@
 require 'date'
 require 'mysql2'
+require_relative 'config/settings'
 
+AGGREGATE = Settings.rate['aggregate']
 ENV['TZ'] = 'UTC'
+CHECKER = AGGREGATE['checker']
+
 now = Time.now
 end_date = (now - now.sec).to_datetime
-
-CHECKER = {
-  :min => [5, 10, 20, 30],
-  :hour => [1, 2, 3, 4, 6, 12],
-  :day => [1],
-  :month => [1],
-}
 
 def check(time_name, date)
   [].tap do |intervals|
@@ -19,17 +16,17 @@ def check(time_name, date)
 end
 
 def min(date)
-  intervals = check(:min, date)
+  intervals = check('min', date)
   intervals.map {|interval| [interval, date - Rational(interval.split('-').first.to_i, 24 * 60)] }
 end
 
 def hour(date)
-  intervals = date.min == 0 ? check(:hour, date) : []
+  intervals = date.min == 0 ? check('hour', date) : []
   intervals.map {|interval| [interval, date - Rational(interval.split('-').first.to_i, 24)] }
 end
 
 def day(date)
-  intervals = (date.min == 0 and date.hour == 0) ? check(:hour, date) : []
+  intervals = (date.min == 0 and date.hour == 0) ? check('hour', date) : []
   intervals.map {|interval| [interval, date - interval.split('-').first.to_i] }
 end
 
@@ -38,24 +35,24 @@ def week(date)
 end
 
 def month(date)
-  intervals = (date.min == 0 and date.hour == 0 and date.day == 1) ? check(:month, date) : []
+  intervals = (date.min == 0 and date.hour == 0 and date.day == 1) ? check('month', date) : []
   intervals.map {|interval| [interval, date << interval.split('-').first.to_i] }
 end
 
 def year(date)
-  intervals = (date.min == 0 and date.hour == 0 and date.day == 1 and date.month == 1) ? check(:year, date) : []
+  intervals = (date.min == 0 and date.hour == 0 and date.day == 1 and date.month == 1) ? check('year', date) : []
   intervals.map {|interval| [interval, date << (12 * interval.split('-').first.to_i)] }
 end
 
-[:min, :hour, :day, :week, :month, :year].each do |time_name|
+%w[ min hour day week month year ].each do |time_name|
   send(time_name, end_date).each do |interval, begin_date|
     query = File.read(File.join(File.dirname(File.absolute_path(__FILE__)), 'rates.aggregate.sql'))
             .gsub('$BEGIN', begin_date.strftime('%Y-%m-%d %H:%M:%S'))
             .gsub('$END', (end_date - Rational(1, 24 * 60 * 60)).strftime('%Y-%m-%d %H:%M:%S'))
             .gsub('$INTERVAL', interval)
-    %w[development production].each do |env|
+    %w[ development production ].each do |env|
       begin
-        client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "7QiSlC?4", :database => "regulus_#{env}")
+        client = Mysql2::Client.new(Settings.mysql.merge('database' => "regulus_#{env}"))
         client.query(query)
         client.close
         puts [
