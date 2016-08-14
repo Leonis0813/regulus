ad4ab3d9ada86eea2b2ba905758551d1f307c35e
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-08-14 17:20:30 +0900

    remove constants from tweet.dump

--- script/tweets.dump.rb
+++ script/tweets.dump.rb
@@ -1,29 +1,34 @@
 require 'date'
 require 'fileutils'
 require 'mysql2'
+require_relative 'config/settings'
 
+DUMP = Settings.tweet['dump']
 ENV['TZ'] = 'UTC'
+
 today = ARGV[0] ? Date.parse(ARGV[0]) : Date.today
 yesterday = today - 1
 
-backup_dir = "backup/tweets/#{yesterday.strftime('%Y-%m')}"
-FileUtils.mkdir_p backup_dir unless File.exists? backup_dir
-csv_file = "#{backup_dir}/#{yesterday.strftime('%d')}.csv"
+backup_dir = File.join(DUMP['backup_dir'], yesterday.strftime('%Y-%m'))
+FileUtils.mkdir_p backup_dir
+csv_file = File.join(backup_dir, "#{yesterday.strftime('%d')}.csv")
 
+from = yesterday.strftime('%Y-%m-%d 00:00:00')
+to = yesterday.strftime('%Y-%m-%d 23:59:59')
 query = <<"EOF"
 SELECT
   *
 FROM
   tweets
 WHERE
-  created_at BETWEEN '#{yesterday.strftime('%Y-%m-%d 00:00:00')}' AND '#{yesterday.strftime('%Y-%m-%d 23:59:59')}'
+  created_at BETWEEN '#{from}' AND '#{to}'
 ORDER BY
   created_at
 EOF
 
 File.open(csv_file, 'w') do |out|
-  out.puts('tweet_id,user_name,profile_image_url,full_text,tweeted_at,created_at')
-  client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "7QiSlC?4", :database => 'regulus')
+  out.puts(DUMP['header'])
+  client = Mysql2::Client.new(Settings.mysql)
   client.query(query).each do |tweet|
     out.puts(tweet.values.join(','))
   end
