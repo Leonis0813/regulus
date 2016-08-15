f0379fca70d59383bc5412e3224f14a94af3de81
Leonis <Leonis.0813@gmail.com>
2016-08-15 04:55:24 +0000

    articles/import use logger

--- script/articles/import.rb
+++ script/articles/import.rb
@@ -1,6 +1,7 @@
 require 'feedjira'
 require 'mysql2'
 require_relative '../config/settings'
+require_relative '../lib/logger'
 require_relative '../lib/mysql_client'
 
 IMPORT = Settings.article['import']
@@ -15,11 +16,7 @@ feed.entries.each do |entry|
   IMPORT['databases'].each do |db|
     begin
       execute_sql(db, File.join(Settings.application_root, 'articles/import.sql'), param)
-      puts [
-        "[#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}]",
-        '[import]',
-        "{database: #{db}, url: #{entry.url}}",
-      ].join(' ')
+      Logger.write('articles', File.basename(__FILE__, '.rb'), {:database => db, :url => entry.url})
     rescue => e
       puts e.message
     end
