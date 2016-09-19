0b9de4ee87e1e9f9f2268b0e556362e6bd83e3d1
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-09-19 22:07:15 +0900

    fix condition

--- script/rates/helper.rb
+++ script/rates/helper.rb
@@ -5,6 +5,8 @@ def out_of_service?
   from, to = Settings.rate['import']['out_of_service']['from'], Settings.rate['import']['out_of_service']['to']
 
   now.saturday? or
-    (now.friday? and now.hour > from['hour'] and now.min > from['minute']) or
-    (now.sunday? and now.hour < to['hour'] and now.min < to['minute'])
+    (now.friday? and now.hour > from['hour']) or
+    (now.friday? and now.hour == from['hour'] and now.min > from['minute']) or
+    (now.sunday? and now.hour < to['hour']) or
+    (now.sunday? and now.hour == to['hour'] and now.min < to['minute'])
 end
