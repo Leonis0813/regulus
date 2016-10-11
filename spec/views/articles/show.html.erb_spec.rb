1f9f2099bad6fa0f893443ea6d4b59422befb75e
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-10-11 20:51:28 +0900

    modify view spec

--- spec/views/articles/show.html.erb_spec.rb
+++ spec/views/articles/show.html.erb_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 
 describe "articles/show", :type => :view do
   include_context 'レスポンス初期化'
+  include_context '記事を作成する', 1
+  before(:each) { assign(:articles, Article.all) }
   include_context 'View: ビューを描画'
 
   it '<div>タグがあること' do
-    expect(@res).to have_selector('div#article')
+    expect(@res).to have_selector('table tbody#article')
   end
 end
