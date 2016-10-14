856979c5b98a95973c3595204e2bbfcf65b68247
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-10-14 20:09:07 +0900

    modify description

--- spec/views/tweets/show.html.erb_spec.rb
+++ spec/views/tweets/show.html.erb_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 describe "tweets/show", :type => :view do
   include_context 'レスポンス初期化'
   include_context 'ツイートを作成する', 1
-  before(:each) { assign(:tweets, Tweet.all) }
+  before(:all) { assign(:tweets, Tweet.all) }
   include_context 'View: ビューを描画'
 
-  it '<div>タグがあること' do
+  it '<tbody>タグがあること' do
     expect(@res).to have_selector('table tbody#tweet')
   end
 end
