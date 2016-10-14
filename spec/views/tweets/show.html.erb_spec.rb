a88d3145643c8d3b35c2abac7ae89675ffb90835
Leonis <Leonis.0813@gmail.com>
2016-10-14 20:23:50 +0900

    refactor (#48)
    * update favicon.ico
    
    * refactor tweet view
    
    * refactor article
    
    * rename scss
    
    * modify view spec
    
    * modify description
    
    * modify requests spec
    
    * modify requests spec

--- spec/views/tweets/show.html.erb_spec.rb
+++ spec/views/tweets/show.html.erb_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 
 describe "tweets/show", :type => :view do
   include_context 'レスポンス初期化'
+  include_context 'ツイートを作成する', 1
+  before(:all) { assign(:tweets, Tweet.all) }
   include_context 'View: ビューを描画'
 
-  it '<div>タグがあること' do
-    expect(@res).to have_selector('div#tweet')
+  it '<tbody>タグがあること' do
+    expect(@res).to have_selector('table tbody#tweet')
   end
 end
