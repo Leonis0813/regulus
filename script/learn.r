97d6c12c4401a650fda24fad18fe52432bc5c5d4
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-10-28 23:49:30 +0900

    get USDJPY

--- script/learn.r
+++ script/learn.r
@@ -1,9 +1,8 @@
-options(repos="http://cran.ism.ac.jp")
 library(RMySQL)
 md <- dbDriver("MySQL")
 dbconnector <- dbConnect(md, dbname="regulus", user="root", password="7QiSlC?4", host="localhost", port=3306)
-sample.table <- dbGetQuery(dbconnector, "select bid from rates order by time desc limit 100")
+rate <- dbGetQuery(dbconnector, "select bid from rates where pair = 'USDJPY' order by time desc limit 1000")
 
 library(forecast)
-model <- auto.arima(sample.table, ic="aic", stepwise=F, approximation=F, start.p=0, start.q=0, start.P=0, start.Q=0)
+model <- auto.arima(rate, ic="aic", stepwise=F, approximation=F, start.p=0, start.q=0, start.P=0, start.Q=0)
 save(model, file="rate.model")
