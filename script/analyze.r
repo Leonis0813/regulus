84ae4588a6ed4a204435bb189571dc61dfbaf9c6
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-10-28 21:50:18 +0900

    predict rates

--- script/analyze.r
+++ script/analyze.r
@@ -1,8 +1,9 @@
 options(repos="http://cran.ism.ac.jp")
-install.packages("DBI")
-install.packages("RMySQL")
 library(RMySQL)
 md <- dbDriver("MySQL")
-dbconnector <- dbConnect(md, dbname="regulus_development", user="root", password="7QiSlC?4", host="160.16.66.112", port=3306)
-sample.table <- dbGetQuery(dbconnector, "select * from rates order by from_date desc limit 50")
-sample.table
+dbconnector <- dbConnect(md, dbname="regulus", user="root", password="7QiSlC?4", host="localhost", port=3306)
+sample.table <- dbGetQuery(dbconnector, "select bid from rates order by time desc limit 100")
+
+library(forecast)
+model <- auto.arima(sample.table, ic="aic", stepwise=F, approximation=F, start.p=0, start.q=0, start.P=0, start.Q=0)
+save(model, file="rate.model")
