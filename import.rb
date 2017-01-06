f5ecb5550af9bf5a7733def672ec1b2e17c8ff82
Leonis0813 <Leonis.0813@gmail.com>
2017-01-07 02:22:50 +0900

    update import.rb

--- import.rb
+++ import.rb
@@ -1,9 +1,28 @@
+require 'csv'
 require 'mysql2'
 require_relative 'config/settings'
+
+day = ARGV[1] ? ARGV[1] : Date.today.strftime('%F')
+
+rates = [].tap do |rates|
+  Dir["/mnt/smb/*_#{day}.csv"].each do |csv|
+    rates += CSV.read(csv, :converters => :all)
+  end
+end
+
+rates.sort_by! {|rate| [rate[0], rate[1]] }
+rates.map! {|rate| [rate[0].strftime('%F %T'), rate[1], rate[2], rate[3]] }
+
+CSV.open('tmp/rate.csv', 'w') do |csv|
+  rates.each do |rate|
+    csv << rate
+  end
+end
+
 client = Mysql2::Client.new(Settings.mysql)
 query =<<"EOF"
 LOAD DATA LOCAL INFILE
-  '/tmp/test.csv'
+  'tmp/rate.csv'
 INTO TABLE
   rates
 FIELDS TERMINATED BY
