4f7d12ef5285e412b9e72a38fc955902ff4f5803
Leonis0813 <Leonis.0813@gmail.com>
2017-01-22 05:22:57 +0000

    remove resource argument

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,7 +1,7 @@
 require_relative '../config/settings'
 
 module Logger
-  def self.write(resource, operate, text)
+  def self.write(operate, text)
     file_path = File.join(Settings.application_root, 'log/aggregate.log')
     body = ["[#{Time.now.strftime('%F %T.%L')}]", "[#{operate}]", text].join(' ')
     File.open(file_path, 'a') {|file| file.puts(body) }
