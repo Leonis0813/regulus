5e1402d24c606daa98353aa7a4e8b2b005cf4a7b
Leonis0813 <Leonis.0813@gmail.com>
2017-03-12 07:00:13 +0000

    for backup

--- lib/mysql_client.rb
+++ lib/mysql_client.rb
@@ -1,8 +1,16 @@
 require 'mysql2'
 
 def import_rates(rate_file)
-    client = Mysql2::Client.new(Settings.mysql)
-    query = File.read(File.join(Settings.application_root, 'aggregate/import.sql'))
-    client.query(query.gsub('$FILE', rate_file))
-    client.close
+  client = Mysql2::Client.new(Settings.mysql)
+  query = File.read(File.join(Settings.application_root, 'aggregate/import.sql'))
+  client.query(query.gsub('$FILE', rate_file))
+  client.close
+end
+
+def get_rates(date)
+  client = Mysql2::Client.new(Settings.mysql)
+  query = File.read(File.join(Settings.application_root, 'aggregate/backup.sql'))
+  result = client.query(query.gsub('$DAY', date.strftime('%F')))
+  client.close
+  result
 end
