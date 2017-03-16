07b215b6fc3ee5643c1893a20c6853f56692cbb6
Leonis0813 <Leonis.0813@gmail.com>
2017-03-16 15:08:55 +0000

    check removed files

--- aggregate/delete.rb
+++ aggregate/delete.rb
@@ -5,7 +5,7 @@ TARGET_DATE = Date.today - 2
 BACKUP_FILE = backup_file(TARGET_DATE)
 REMOVED_FILES = rate_files(TARGET_DATE)
 
-if File.exists?(BACKUP_FILE)
+if File.exists?(BACKUP_FILE) and not REMOVED_FILES.empty?
   rates_count = REMOVED_FILES.inject {|count, csv| File.read(csv).lines.size }
   FileUtils.rm(REMOVED_FILES) if rates_count == File.read(BACKUP_FILE).lines.size
 end
