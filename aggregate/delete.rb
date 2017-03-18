43099130227e8f7dee5e707e0c20a4d83c050bce
Leonis <Leonis.0813@gmail.com>
2017-03-18 13:12:15 +0900

    Fix bug delete (#68)

--- aggregate/delete.rb
+++ aggregate/delete.rb
@@ -6,6 +6,6 @@ BACKUP_FILE = backup_file(TARGET_DATE)
 REMOVED_FILES = rate_files(TARGET_DATE)
 
 if File.exists?(BACKUP_FILE) and not REMOVED_FILES.empty?
-  rates_count = REMOVED_FILES.inject {|count, csv| count + File.read(csv).lines.size }
+  rates_count = REMOVED_FILES.inject(0) {|count, csv| count + File.read(csv).lines.size }
   FileUtils.rm(REMOVED_FILES) if rates_count == File.read(BACKUP_FILE).lines.size
 end
