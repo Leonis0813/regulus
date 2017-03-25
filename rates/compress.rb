fda0b8123ac8b58f5326af70feccb7c209039d99
Leonis0813 <Leonis.0813@gmail.com>
2017-03-25 13:12:50 +0000

    remove old script

--- rates/compress.rb
+++
@@ -1,4 +0,0 @@
-require 'date'
-
-last_month = (Date.today -2).strftime('%Y-%m')
-system "cd backup/rates; tar zcf #{last_month}.tar.gz #{last_month}"
--- rates/dump.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  *
-FROM
-  rates
-WHERE
-  time BETWEEN '$FROM' AND '$TO'
-ORDER BY
-  time
