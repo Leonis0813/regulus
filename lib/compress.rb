83fe20ee07cc8f2a61638e594ff2d0fa4df4a9f5
Leonis0813 <Leonis.0813@gmail.com>
2017-03-25 14:27:59 +0000

    refactor & fix bug

--- lib/compress.rb
+++ lib/compress.rb
@@ -1,23 +1,23 @@
 require 'fileutils'
 require 'minitar'
 require 'zlib'
+require_relative '../config/settings'
 
-TARGET_MONTH = Date.today - 1
+TARGET_MONTH = (Date.today << 1).strftime('%Y-%m')
+TMP_DIR = File.join(Settings.application_root, 'tmp')
+COMPRESSED_DIR = File.join(TMP_DIR, TARGET_MONTH)
+BACKUP_DIR = File.join(Settings.application_root, 'backup')
 
-FileUtils.mkdir_p("tmp/#{TARGET_MONTH.strftime('%Y%m')}")
+FileUtils.mkdir_p(COMPRESSED_DIR)
 
-Zlib::GzipWriter.open("#{TARGET_MONTH.strftime('%Y%m')}.tar.gz", Zlib::BEST_COMPRESSION) do |gz|
+Zlib::GzipWriter.open(File.join(BACKUP_DIR, "#{TARGET_MONTH}.tar.gz"), Zlib::BEST_COMPRESSION) do |gz|
   out = Minitar::Output.new(gz)
 
-  FileUtils.cp(Dir["backup/#{TARGET_MONTH.strftime('%Y-%m')}-*.csv"], "tmp/#{TARGET_MONTH.strftime('%Y%m')}")
-
-  Dir["tmp/#{TARGET_MONTH.strftime('%Y%m')}/*"].each do |file|
-    Minitar::pack_file(file, out)
-  end
-
-  FileUtils.rm_rf("tmp/#{TARGET_MONTH.strftime('%Y%m')}")
+  FileUtils.cp(Dir[File.join(BACKUP_DIR, "#{TARGET_MONTH}-*.csv")], COMPRESSED_DIR)
+  Dir::chdir(TMP_DIR)
+  Dir["#{TARGET_MONTH}/*"].each {|file| Minitar::pack_file(file, out) }
 
   out.close
 end
 
-FileUtils.mv("#{TARGET_MONTH.strftime('%Y%m')}.tar.gz", 'backup')
+FileUtils.rm_rf(COMPRESSED_DIR)
