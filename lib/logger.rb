c952897caccf15c14994866948b7470a33893f48
Leonis0813 <Leonis.0813@gmail.com>
2017-04-16 06:11:51 +0100

    refactor

--- lib/logger.rb
+++ lib/logger.rb
@@ -3,18 +3,18 @@ require_relative '../config/settings'
 module Logger
   FILE_PATH = File.join(Settings.application_root, 'log/aggregate.log')
 
-  def self.write(text)
-    operate = File.basename(caller[0][/^([^:]+):\d+:in `[^']*'$/, 1], '.rb')
-    body = ["[#{Time.now.strftime('%F %T.%6N')}]", "[#{operate}]", text.to_s].join(' ')
-    File.open(FILE_PATH, 'a') {|file| file.puts(body) }
-  end
+  class << self
+    def write(text)
+      operate = File.basename(caller[0][/^([^:]+):\d+:in `[^']*'$/, 1], '.rb')
+      body = ["[#{Time.now.strftime('%F %T.%6N')}]", "[#{operate}]", text.to_s].join(' ')
+      File.open(FILE_PATH, 'a') {|file| file.puts(body) }
+    end
 
-  def self.write_with_runtime
-    start_time = Time.now
-    text = yield
-    end_time = Time.now
-    operate = File.basename(caller[0][/^([^:]+):\d+:in `[^']*'$/, 1], '.rb')
-    body = ["[#{Time.now.strftime('%F %T.%6N')}]", "[#{operate}]", text.merge('runtime' => end_time - start_time).to_s].join(' ')
-    File.open(FILE_PATH, 'a') {|file| file.puts(body) }
+    def write_with_runtime
+      start_time = Time.now
+      text = yield
+      end_time = Time.now
+      write text.merge('runtime' => end_time - start_time)
+    end
   end
 end
