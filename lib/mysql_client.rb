5fe0729c1492ebeb8f4d997e978a42284f5b2734
Leonis0813 <Leonis.0813@gmail.com>
2017-04-17 13:36:09 +0100

    create MySQLClient class

--- lib/mysql_client.rb
+++ lib/mysql_client.rb
@@ -1,28 +1,46 @@
 require 'mysql2'
+require_relative 'logger'
+require_relative '../config/settings'
 
-def import_rates(rate_file)
-  client = Mysql2::Client.new(Settings.mysql)
-  query = File.read(File.join(Settings.application_root, 'aggregate/import.sql'))
-  client.query(query.gsub('$FILE', rate_file))
-  client.close
-end
+class MySQLClient
+  SQL_PATH = File.join(Settings.application_root, 'aggregate')
 
-def get_rates(date)
-  client = Mysql2::Client.new(Settings.mysql)
-  query = File.read(File.join(Settings.application_root, 'aggregate/backup.sql'))
-  result = client.query(query.gsub('$DAY', date.strftime('%F')))
-  client.close
-  result
-end
+  def initialize
+    @client = Mysql2::Client.new(Settings.mysql)
+  end
 
-def create_candle_sticks(param)
-  begin
-    client = Mysql2::Client.new(Settings.mysql)
+  def import_rates(rate_file)
+    query = File.read(File.join(SQL_PATH, 'import.sql'))
+
+    Logger.write_with_runtime(:param => {:file => rate_file}) do
+      execute_query(query.gsub('$FILE', rate_file))
+    end
+  end
+
+  def get_rates(date)
+    query = File.read(File.join(SQL_PATH, 'backup.sql'))
+    day = date.strftime('%F')
+
+    Logger.write_with_runtime(:param => {:day => day}) do
+      execute_query(query.gsub('$DAY', day))
+    end
+  end
+
+  def create_candle_sticks(param)
     query = File.read(File.join(Settings.application_root, 'aggregate.sql'))
     param.each {|key, value| query.gsub!("$#{key.upcase}", value) }
-    client.query(query)
-  rescue
-  ensure
-    client.close
+
+    Logger.write_with_runtime(:param => param) do
+      execute_query(query)
+    end
+  end
+
+  private
+
+  def execute_query(query)
+    begin
+      @client.query(query)
+    rescue
+    end
   end
 end
