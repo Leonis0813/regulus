1e2d1fa261cb369d4085689e2b56ffe33c0514e7
Leonis0813 <Leonis.0813@gmail.com>
2017-06-03 21:12:34 +0900

    get training data from db

---
+++ analyze/learn.r
@@ -0,0 +1,10 @@
+library(yaml)
+config <- yaml.load_file("analyze/settings.yml")
+
+library(RMySQL)
+driver <- dbDriver("MySQL")
+dbconnector <- dbConnect(driver, dbname="regulus", user=config$mysql$user, password=config$mysql$password, host=config$mysql$host, port=as.integer(config$mysql$port))
+
+sql <- paste("SELECT * FROM rates LIMIT 10000")
+training_data <- dbGetQuery(dbconnector, sql)
+print(training_data)
