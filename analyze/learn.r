8ddf8ac204f803b7e7ee5da1c5b20e2567603ce3
Leonis0813 <Leonis.0813@gmail.com>
2017-08-27 16:41:20 +0900

    create label

--- analyze/learn.r
+++ analyze/learn.r
@@ -5,5 +5,22 @@ library(RMySQL)
 driver <- dbDriver("MySQL")
 dbconnector <- dbConnect(driver, dbname="regulus", user=config$mysql$user, password=config$mysql$password, host=config$mysql$host, port=as.integer(config$mysql$port))
 
-sql <- paste("SELECT time, ask, bid FROM rates WHERE pair = 'USDJPY' LIMIT 200")
+sql <- paste("SELECT time, ask, bid FROM rates WHERE pair = 'USDJPY' ORDER BY time LIMIT 200")
 rates <- dbGetQuery(dbconnector, sql)
+
+latest = as.POSIXlt(rates$time[length(rates$time)])
+training_data = list(x = rates$bid[as.POSIXlt(rates$time) < latest - 300])
+training_data$y = rep(0, length(training_data$x))
+
+for (i in 1:length(training_data$x)) {
+  after_5_min = as.POSIXlt(rates$time[i]) + 300
+  rate_after_5 = rates$bid[as.POSIXlt(rates$time) == after_5_min]
+
+  if(length(rate_after_5) == 0) {
+    training_data$y[i] = 0
+  } else if(rate_after_5[1] > rates$bid[i]) {
+    training_data$y[i] = 1
+  }  else {
+    training_data$y[i] = 0
+  }
+}
