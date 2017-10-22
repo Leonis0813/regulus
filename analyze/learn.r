481d8970e6e5f1a7a30a8ff996efa55adb4e35c5
Leonis0813 <Leonis.0813@gmail.com>
2017-10-22 05:08:15 +0100

    save as yaml

--- analyze/learn.r
+++ analyze/learn.r
@@ -9,18 +9,26 @@ library(RMySQL)
 driver <- dbDriver("MySQL")
 dbconnector <- dbConnect(driver, dbname="regulus", user=config$mysql$user, password=config$mysql$password, host=config$mysql$host, port=as.integer(config$mysql$port))
 
-sql <- paste("SELECT ask FROM rates WHERE pair = 'USDJPY' ORDER BY time LIMIT 200")
+sql <- paste("SELECT close FROM candle_sticks WHERE pair = 'USDJPY' AND `interval` = '1-min' ORDER BY id LIMIT 200")
 rates <- dbGetQuery(dbconnector, sql)
 
 x <- matrix(0, nrow=num_training_data, ncol=length)
 y <- rep(0, num_training_data)
 
 for (i in 1:num_training_data) {
-  x[i,] <- rates$ask[i : (i + length - 1)]
-  y[i] <- rates$ask[i + period + length - 1]
+  x[i,] <- rates$close[i : (i + length - 1)]
+  y[i] <- rates$close[i + period + length - 1]
 }
 
 model = lm(y ~ x[,1] + x[,2] + x[,3] + x[,4] + x[,5] + x[,6] + x[,7] + x[,8] + x[,9] + x[,10])
 
 timestamp <- format(Sys.time(), "%Y%m%d%H%M%S")
-save(model, file=paste("results/", timestamp, ".lm", sep=""))
+
+basename <- paste(timestamp, "200", period, length, sep="_")
+yml_filename <- paste("results/", basename, ".yml", sep="")
+write("coefficients:", file=paste(yml_filename))
+coefs <- as.vector(coef(model))
+for (i in 2:(length+1)) {
+  write(paste("  -", coefs[i]), file=yml_filename, append=T)
+}
+write(paste("constant:", coefs[1]), file=yml_filename, append=T)
