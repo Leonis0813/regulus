dcbde7b19fb1a170bd41b3cb557f7e3c5be17079
Leonis0813 <Leonis.0813@gmail.com>
2017-10-22 05:17:12 +0100

    use arguments

--- analyze/learn.r
+++ analyze/learn.r
@@ -1,6 +1,7 @@
-period <- 10
+args = commandArgs(trailingOnly=T)
+num_training_data <- as.integer(args[1])
+period <- as.integer(args[2])
 length <- 10
-num_training_data <- 200 - (period + length)
 
 library(yaml)
 config <- yaml.load_file("analyze/settings.yml")
@@ -9,13 +10,13 @@ library(RMySQL)
 driver <- dbDriver("MySQL")
 dbconnector <- dbConnect(driver, dbname="regulus", user=config$mysql$user, password=config$mysql$password, host=config$mysql$host, port=as.integer(config$mysql$port))
 
-sql <- paste("SELECT close FROM candle_sticks WHERE pair = 'USDJPY' AND `interval` = '1-min' ORDER BY id LIMIT 200")
+sql <- paste("SELECT close FROM candle_sticks WHERE pair = 'USDJPY' AND `interval` = '1-min' ORDER BY id LIMIT", num_training_data, sep=" ")
 rates <- dbGetQuery(dbconnector, sql)
 
-x <- matrix(0, nrow=num_training_data, ncol=length)
-y <- rep(0, num_training_data)
+x <- matrix(0, nrow=(num_training_data - (period + length)), ncol=length)
+y <- rep(0, num_training_data - (period + length))
 
-for (i in 1:num_training_data) {
+for (i in 1:length(y)) {
   x[i,] <- rates$close[i : (i + length - 1)]
   y[i] <- rates$close[i + period + length - 1]
 }
@@ -24,7 +25,7 @@ model = lm(y ~ x[,1] + x[,2] + x[,3] + x[,4] + x[,5] + x[,6] + x[,7] + x[,8] + x
 
 timestamp <- format(Sys.time(), "%Y%m%d%H%M%S")
 
-basename <- paste(timestamp, "200", period, length, sep="_")
+basename <- paste(timestamp, num_training_data, period, sep="_")
 yml_filename <- paste("results/", basename, ".yml", sep="")
 write("coefficients:", file=paste(yml_filename))
 coefs <- as.vector(coef(model))
