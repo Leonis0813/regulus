5703198f2198e6e8033943af970697724ebea9fe
Leonis0813 <Leonis.0813@gmail.com>
2017-10-22 04:44:31 +0100

    use lm for rates

--- analyze/learn.r
+++ analyze/learn.r
@@ -1,3 +1,7 @@
+period <- 10
+length <- 10
+num_training_data <- 200 - (period + length)
+
 library(yaml)
 config <- yaml.load_file("analyze/settings.yml")
 
@@ -5,41 +9,18 @@ library(RMySQL)
 driver <- dbDriver("MySQL")
 dbconnector <- dbConnect(driver, dbname="regulus", user=config$mysql$user, password=config$mysql$password, host=config$mysql$host, port=as.integer(config$mysql$port))
 
-sql <- paste("SELECT time, ask, bid FROM rates WHERE pair = 'USDJPY' ORDER BY time LIMIT 200")
+sql <- paste("SELECT ask FROM rates WHERE pair = 'USDJPY' ORDER BY time LIMIT 200")
 rates <- dbGetQuery(dbconnector, sql)
 
-latest = as.POSIXlt(rates$time[length(rates$time)])
-training_data = list(x = rates$bid[as.POSIXlt(rates$time) < latest - 300])
-training_data$y = rep(0, length(training_data$x))
+x <- matrix(0, nrow=num_training_data, ncol=length)
+y <- rep(0, num_training_data)
 
-for (i in 1:length(training_data$x)) {
-  after_5_min = as.POSIXlt(rates$time[i]) + 300
-  future_rates = rates$bid[as.POSIXlt(rates$time) == after_5_min]
-  if(length(future_rates) == 0) {
-    j = 0
-    while(TRUE) {
-      before_x_min = as.POSIXlt(rates$time[i]) + 300 - j
-      after_x_min = as.POSIXlt(rates$time[i]) + 300 + j
-      future_rates = rates$bid[before_x_min < as.POSIXlt(rates$time) & as.POSIXlt(rates$time) < after_x_min]
-
-      if(length(future_rates) > 0) {
-        break
-      }
-      j = j + 1
-    }
-  }
-
-  if(future_rates[1] > rates$bid[i]) {
-    training_data$y[i] = 1
-  }  else {
-    training_data$y[i] = 0
-  }
+for (i in 1:num_training_data) {
+  x[i,] <- rates$ask[i : (i + length - 1)]
+  y[i] <- rates$ask[i + period + length - 1]
 }
 
-training_data$y = as.factor(training_data$y)
-
-library(randomForest)
-model <- randomForest(y~., data=training_data, ntree=500, mtry=1)
+model = lm(y ~ x[,1] + x[,2] + x[,3] + x[,4] + x[,5] + x[,6] + x[,7] + x[,8] + x[,9] + x[,10])
 
 timestamp <- format(Sys.time(), "%Y%m%d%H%M%S")
-save(model, file=paste("results/", timestamp, ".rf", sep=""))
+save(model, file=paste("results/", timestamp, ".lm", sep=""))
