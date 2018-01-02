24179f0d98bf830db277d747327bfe55828eb758
Leonis0813 <Leonis.0813@gmail.com>
2018-01-02 08:38:02 +0000

    update model

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,8 +1,5 @@
-class Analysis
-  include ActiveModel::Model
-
-  attr_accessor :num_data, :interval
-
+class Analysis < ActiveRecord::Base
   validates :num_data, :numericality => {:only_integer => true, :greater_than => 0}
   validates :interval, :numericality => {:only_integer => true, :greater_than => 0}
+  validates :state, :inclusion => {:in => %w[ processing completed ]}
 end
