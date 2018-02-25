7b76be0fd531a7504686106becf9ad4defbdb124
Leonis <Leonis.0813@gmail.com>
2018-02-25 16:30:26 +0900

    Create system test (#87)

--- spec/requests/manage_analyses_spec.rb
+++ spec/requests/manage_analyses_spec.rb
@@ -1,14 +1,17 @@
 # coding: utf-8
 require 'rails_helper'
 
-describe "ブラウザで管理する", :type => :request do
-  before(:all) { @driver = Selenium::WebDriver.for :firefox }
+describe 'ブラウザで管理する', :type => :request do
+  before(:all) do
+    @driver = Selenium::WebDriver.for :firefox
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 30)
+  end
 
   describe '管理画面を開く' do
-    before(:all) { @driver.get("#{base_url}/analysis") }
+    before(:all) { @driver.get("#{base_url}/analyses") }
 
     it '管理画面が表示されていること' do
-      is_asserted_by { @driver.current_url == "#{base_url}/analysis" }
+      is_asserted_by { @driver.current_url == "#{base_url}/analyses" }
     end
   end
 
@@ -17,7 +20,7 @@ describe "ブラウザで管理する", :type => :request do
       @driver.find_element(:id, 'analysis_num_data').send_keys('invalid')
       @driver.find_element(:id, 'analysis_interval').send_keys(1)
       @driver.find_element(:xpath, '//form/span/input[@value="実行"]').click
-      sleep 1
+      @wait.until { @driver.find_element(:class, 'modal-body').displayed? }
     end
 
     it 'タイトルが正しいこと' do
