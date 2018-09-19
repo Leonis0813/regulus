a0a9e49100311dd5358e78514c5e3f6f6918cdd1
Leonis0813 <Leonis.0813@gmail.com>
2018-09-19 12:18:20 +0100

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,37 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102083542) do
+ActiveRecord::Schema.define(version: 20180919111425) do
 
   create_table "analyses", force: :cascade do |t|
-    t.integer  "num_data",   limit: 4
-    t.integer  "interval",   limit: 4
+    t.datetime "from",                   null: false
+    t.datetime "to",                     null: false
+    t.integer  "batch_size", limit: 4,   null: false
     t.string   "state",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
-
-  create_table "candle_sticks", force: :cascade do |t|
-    t.datetime "from",                null: false
-    t.datetime "to",                  null: false
-    t.string   "pair",     limit: 6,  null: false
-    t.string   "interval", limit: 8,  null: false
-    t.float    "open",     limit: 24, null: false
-    t.float    "close",    limit: 24, null: false
-    t.float    "high",     limit: 24, null: false
-    t.float    "low",      limit: 24, null: false
-  end
-
-  add_index "candle_sticks", ["from", "to", "pair"], name: "from", unique: true, using: :btree
-
-  create_table "rates", force: :cascade do |t|
-    t.datetime "time",            null: false
-    t.string   "pair", limit: 6,  null: false
-    t.float    "bid",  limit: 24, null: false
-    t.float    "ask",  limit: 24, null: false
-  end
-
-  add_index "rates", ["pair"], name: "index_pair", using: :btree
-  add_index "rates", ["time"], name: "index_time", using: :btree
 
 end
