b9902a2c56c8fdd200c752cd0f3052eb58092fdd
Leonis0813 <Leonis.0813@gmail.com>
2018-10-14 01:24:51 +0000

    update models spec

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,25 +1,17 @@
 class Analysis < ActiveRecord::Base
   validate :valid_period?
-  validates :batch_size, :numericality => {:only_integer => true, :greater_than => 0}
-  validates :state, :inclusion => {:in => %w[ processing completed ]}
+  validates :batch_size, :numericality => {:only_integer => true, :greater_than => 0, :message => 'invalid'}
+  validates :state, :inclusion => {:in => %w[ processing completed ], :message => 'invalid'}
 
   private
 
   def valid_period?
-    from ||= Time.at(0).strftime('%F %T')
-    to ||= Time.now.strftime('%F %T')
-
-    [[:from, from], [:to, to]].each do |key, value|
-      begin
-        Time.parse(value)
-      rescue ArgumentError => e
-        errors.add(key, 'invalid')
-      end
-    end
+    errors.add(:from, 'invalid') unless from
+    errors.add(:to, 'invalid') unless to
 
     return if errors.messages.include?(:from) or errors.messages.include?(:to)
 
-    unless Time.parse(from) < Time.parse(to)
+    unless from < to
       errors.add(:from, 'invalid')
       errors.add(:to, 'invalid')
     end
--- spec/models/analysis_spec.rb
+++ spec/models/analysis_spec.rb
@@ -13,23 +13,61 @@ describe Analysis, :type => :model do
     it_is_asserted_by { @analysis.errors.empty? == result }
   end
 
+  shared_examples 'エラーメッセージが正しいこと' do |expected_messages|
+    it_is_asserted_by { @analysis.errors.messages == expected_messages }
+  end
+
   describe '#validates' do
     describe '正常系' do
-      include_context 'Analysisオブジェクトを検証する', {:num_data => 1, :interval => 1, :state => 'processing'}
-      it_behaves_like '検証結果が正しいこと', true
+      valid_params = {
+        :from => %w[ 1000-01-01 1000/01/01 01-01-1000 01/01/1000 10000101 ],
+        :to => %w[ 1001-01-01 1001/01/01 01-01-1001 01/01/1001 10010101 ],
+        :batch_size => 100,
+        :state => %w[ processing completed ],
+      }
+
+      test_cases = CommonHelper.generate_test_case(valid_params).select do |test_case|
+        test_case.keys == valid_params.keys
+      end
+
+      test_cases.each do|params|
+        include_context 'Analysisオブジェクトを検証する', params
+        it_behaves_like '検証結果が正しいこと', true
+      end
     end
 
     describe '異常系' do
+      valid_params = {:from => '1000-01-01', :to => '1001-01-01', :batch_size => 100, :state => 'processing'}
       invalid_params = {
-        :num_data => ['invalid', 1.0, 0],
-        :interval => ['invalid', 1.0, 0],
+        :from => ['invalid', '1000-13-01', '1000-01-00', '1000-13-00', 1.0, 0, true, nil],
+        :to => ['invalid', '1000-13-01', '1000-01-00', '1000-13-00', 1.0, 0, true, nil],
+        :batch_size => ['invalid', -1, 0, true, nil],
         :state => ['invalid', 1.0, 0],
       }
 
       CommonHelper.generate_test_case(invalid_params).each do |params|
         context "フォームに#{params.keys.join(',')}を指定した場合" do
-          include_context 'Analysisオブジェクトを検証する', params
+          include_context 'Analysisオブジェクトを検証する', valid_params.merge(params)
           it_behaves_like '検証結果が正しいこと', false
+          it_behaves_like 'エラーメッセージが正しいこと', params.map {|key, _| [key, ['invalid']] }.to_h
+        end
+      end
+
+      invalid_period = {
+        :from => %w[ 1000-01-02 1000/01/02 02-01-1000 02/01/1000 10000102 ],
+        :to => %w[ 1000-01-01 1000/01/01 01-01-1000 01/01/1000 10000101 ],
+      }
+
+      test_cases = CommonHelper.generate_test_case(invalid_params.merge(invalid_period)).select do |test_case|
+        test_case.include?(:from) and test_case.include?(:to)
+      end
+
+      test_cases.each do |params|
+        context '期間が不正な場合' do
+          include_context 'Analysisオブジェクトを検証する', valid_params.merge(params)
+
+          it_behaves_like '検証結果が正しいこと', false
+          it_behaves_like 'エラーメッセージが正しいこと', params.map {|key, _| [key, ['invalid']] }.to_h
         end
       end
     end
