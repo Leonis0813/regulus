2250fd5a38dfe674472015089ef775d5ffa27bd5
Leonis0813 <Leonis.0813@gmail.com>
2018-11-24 04:57:35 +0000

    modify script path

--- app/jobs/analysis_job.rb
+++ app/jobs/analysis_job.rb
@@ -8,10 +8,10 @@ class AnalysisJob < ActiveJob::Base
       "'#{analysis.to.strftime('%F %T')}'",
       analysis.batch_size,
     ]
-    script_dir = '/opt/regulus'
+    script_dir = '/opt/scripts'
     FileUtils.mkdir(File.join(script_dir, 'tmp'))
     ret = system "sudo docker exec regulus python #{File.join(script_dir, 'learn.py')} #{args.join(' ')}"
-    FileUtils.mv(File.join(script_dir, 'tmp'), File.join(Rails.root, 'tmp'. analysis_id)))
+    FileUtils.mv(File.join(script_dir, 'tmp'), File.join(Rails.root, 'tmp/models', analysis_id))
     analysis.update!(:state => 'completed')
     AnalysisMailer.finished(analysis, ret).deliver_now
   end
