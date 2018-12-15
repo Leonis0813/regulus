35c50dd850b109de3f562c124450cf1c7c2e8e6b
Leonis0813 <Leonis.0813@gmail.com>
2018-12-15 11:39:32 +0000

    attach analysis.zip

--- app/mailers/analysis_mailer.rb
+++ app/mailers/analysis_mailer.rb
@@ -6,6 +6,18 @@ class AnalysisMailer < ApplicationMailer
     @analysis = analysis
     subject = is_success ? '分析が完了しました' : '分析中にエラーが発生しました'
     template_name = is_success ? 'success' : 'failer'
+    tmp_dir = File.join(Rails.root, "tmp/models/#{analysis.id}")
+
+    file_names = %w[ model.ckpt ]
+    zip_file_name = File.join(tmp_dir, 'analysis.zip')
+
+    Zip::File.open(zip_file_name, Zip::File::CREATE) do |zip|
+      file_names.each do |file_name|
+        zip.add(file_name, File.join(tmp_dir, file_name))
+      end
+    end
+
+    attachments['analysis.zip'] = File.read(zip_file_name)
     mail(:to => 'Leonis.0813@gmail.com', :subject => subject, :template_name => template_name)
   end
 end
