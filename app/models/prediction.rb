10d042d9b72b9c1c4b28b81a2556483d634dadc0
Leonis0813 <Leonis.0813@gmail.com>
2018-12-26 12:08:27 +0000

    create prediction model spec and fix bug

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,23 +1,12 @@
 class Prediction < ActiveRecord::Base
   validate :valid_period?
-  validates :result, :inclusion => {:in => %w[ up down range ], :message => 'invalid'}
+  validates :result, :inclusion => {:in => %w[ up down range ], :message => 'invalid'}, :allow_nil => true
   validates :state, :inclusion => {:in => %w[ processing completed ], :message => 'invalid'}
 
   private
 
   def valid_period?
-    [
-      [from, :from],
-      [to, :to],
-    ].each do |time, attribute|
-      begin
-        Time.parse(time) if time
-      rescue ArgumentError => e
-        errors.add(attribute, 'invalid')
-      end
-    end
-
-    return if errors.messages.include?(:from) or errors.messages.include?(:to)
+    return unless from and to
 
     unless from < to
       errors.add(:from, 'invalid')
