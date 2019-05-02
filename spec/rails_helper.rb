03c00ad752d48caacff0079dff2ea0c0798ba963
Leonis0813 <Leonis.0813@gmail.com>
2019-05-02 11:17:29 +0000

    coverage setting

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails' do
+  SimpleCov.command_name Time.now.to_s
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
