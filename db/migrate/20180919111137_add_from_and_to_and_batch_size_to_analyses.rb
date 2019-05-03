fc6aad173f4018fbd6aef411e011388f5136bc4a
Leonis0813 <Leonis.0813@gmail.com>
2019-05-03 10:45:11 +0000

    fix Rails/NotNullColumn

--- db/migrate/20180919111137_add_from_and_to_and_batch_size_to_analyses.rb
+++ db/migrate/20180919111137_add_from_and_to_and_batch_size_to_analyses.rb
@@ -1,7 +1,7 @@
 class AddFromAndToAndBatchSizeToAnalyses < ActiveRecord::Migration
   def change
-    add_column :analyses, :from, :datetime, after: :id, null: false
-    add_column :analyses, :to, :datetime, after: :from, null: false
-    add_column :analyses, :batch_size, :integer, after: :to, null: false
+    add_column :analyses, :from, :datetime, after: :id, null: false, default: Time.at(0)
+    add_column :analyses, :to, :datetime, after: :from, null: false, default: Time.at(10**10)
+    add_column :analyses, :batch_size, :integer, after: :to, null: false, default: 0
   end
 end
