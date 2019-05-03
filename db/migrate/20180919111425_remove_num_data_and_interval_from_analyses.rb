16579d1e87c6bf60dd74c2844bcc50a1a818d181
Leonis0813 <Leonis.0813@gmail.com>
2019-05-03 10:55:17 +0000

    fix Rails/BulkChangeTable

--- db/migrate/20180919111425_remove_num_data_and_interval_from_analyses.rb
+++ db/migrate/20180919111425_remove_num_data_and_interval_from_analyses.rb
@@ -1,6 +1,8 @@
 class RemoveNumDataAndIntervalFromAnalyses < ActiveRecord::Migration
   def change
-    remove_column :analyses, :num_data
-    remove_column :analyses, :interval
+    change_table :analyses, bulk: true do |t|
+      t.remove :num_data
+      t.remove :interval
+    end
   end
 end
