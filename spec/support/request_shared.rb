7ef984c66139ec80edeeeb06e92495ed2e830eda
Leonis <Leonis.0813@gmail.com>
2019-06-02 10:54:27 +0900

    Fix request spec (#128)

--- spec/support/request_shared.rb
+++ spec/support/request_shared.rb
@@ -1,14 +1,19 @@
 # coding: utf-8
 
 shared_context 'WebDriverを起動してCookieをセットする' do
-  user_id = 'test_user_id'
-  password = 'test_user_pass'
+  test_user_id = 'test_user_id'
+  test_password = 'test_user_pass'
 
   before(:all) do
-    @driver ||= Selenium::WebDriver.for :firefox
-    @driver.get("#{base_url}/404_path")
-    cookie_value = Base64.strict_encode64("#{user_id}:#{password}")
-    @driver.manage.add_cookie(name: 'algieba', value: cookie_value)
-    @wait ||= Selenium::WebDriver::Wait.new(timeout: 30)
+    @driver = Selenium::WebDriver.for :firefox
+    @wait = Selenium::WebDriver::Wait.new(timeout: 30)
+
+    @driver.get("#{base_url.sub('/regulus', '')}/login.html")
+    user_id = @wait.until { @driver.find_element(:id, 'user_id') }
+    user_id.send_keys(test_user_id)
+    password = @wait.until { @driver.find_element(:id, 'password') }
+    password.send_keys(test_password)
+    @driver.find_element(:xpath, '//button[@type="submit"]').click
+    @wait.until { @driver.manage.cookie_named('LSID') }
   end
 end
