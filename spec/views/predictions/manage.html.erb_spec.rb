1ba0660b4960b2aad9ddd26241139cd93685fa1f
Leonis0813 <Leonis.0813@gmail.com>
2019-06-08 08:02:12 +0000

    update prediction view spec

--- spec/views/predictions/manage.html.erb_spec.rb
+++ spec/views/predictions/manage.html.erb_spec.rb
@@ -4,23 +4,42 @@ require 'rails_helper'
 
 describe 'predictions/manage', type: :view do
   per_page = 1
+  default_attribute = {model: 'analysis.zip', state: 'processing'}
+  icon_class = {
+    'processing' => 'question-sign',
+    'error' => 'remove',
+    'completed' => {
+      'up' => 'circle-arrow-up',
+      'down' => 'circle-arrow-down',
+      'range' => 'circle-arrow-right',
+    },
+  }
+  icon_color = {
+    'processing' => 'black',
+    'error' => 'red',
+    'completed' => {
+      'up' => 'blue',
+      'down' => 'red',
+      'range' => 'orange',
+    },
+  }
 
-  shared_context '予測ジョブを登録する' do |num|
+  shared_context '予測ジョブを登録する' do |total: per_page, attribute: default_attribute|
     before(:all) do
-      num.times do
-        prediction = Prediction.new(
-          model: 'analysis.zip',
-          state: %w[processing completed].sample,
-        )
-        prediction.result = %w[up down range].sample if prediction.state == 'completed'
-        prediction.save!
-      end
+      total.times { Prediction.create!(attribute) }
       @predictions = Prediction.order(created_at: :desc).page(1)
     end
+  end
 
-    after(:all) do
-      Prediction.destroy_all
-    end
+  shared_examples '画面共通テスト' do |expected: {}|
+    it_behaves_like 'ヘッダーが表示されていること'
+    it_behaves_like '入力フォームが表示されていること'
+    it_behaves_like '表示件数情報が表示されていること',
+                    total: expected[:total] || per_page,
+                    from: expected[:from] || 1,
+                    to: expected[:to] || per_page
+    it_behaves_like 'テーブルが表示されていること',
+                    rows: expected[:rows] || per_page
   end
 
   shared_examples '入力フォームが表示されていること' do
@@ -45,72 +64,48 @@ describe 'predictions/manage', type: :view do
     end
   end
 
-  shared_examples 'ジョブ実行履歴が表示されていること' do |expected: {}|
-    base_xpath = '/html/body/div[@id="main-content"]/div[@class="row center-block"]' \
-                 '/div[@class="col-lg-8"]'
-
-    it 'タイトルが表示されていること' do
-      expect(@html).to have_selector("#{base_xpath}/h4", text: 'ジョブ実行履歴')
+  shared_examples 'テーブルが表示されていること' do |rows: 0|
+    before(:each) do
+      @table = @html.xpath("#{table_panel_xpath}/table[@class='table table-hover']")
     end
 
-    it '件数情報が表示されていること' do
-      text = "#{expected[:total]}件中#{expected[:from]}〜#{expected[:to]}件を表示"
-      expect(@html).to have_selector("#{base_xpath}/h4", text: text)
+    it '4列のテーブルが表示されていること' do
+      is_asserted_by { @table.xpath('//thead/th').size == 4 }
     end
 
-    paging_xpath = "#{base_xpath}/nav/ul[@class='pagination']"
-
-    it '先頭のページへのボタンが表示されていないこと' do
-      xpath = "#{paging_xpath}/li[@class='pagination']/span[@class='first']/a"
-      text = I18n.t('views.list.pagination.first')
-      expect(@html).not_to have_selector(xpath, text: text)
-    end
-
-    it '前のページへのボタンが表示されていないこと' do
-      xpath = "#{paging_xpath}/li[@class='pagination']/span[@class='prev']/a"
-      text = I18n.t('views.list.pagination.previous')
-      expect(@html).not_to have_selector(xpath, text: text)
-    end
-
-    it '1ページ目が表示されていること' do
-      xpath = "#{paging_xpath}/li[@class='page-item active']"
-      expect(@html).to have_selector(xpath, text: 1)
-    end
-
-    it '2ページ目が表示されていること' do
-      xpath = "#{paging_xpath}/li[@class='page-item']/a[href='/predictions?page=2']"
-      expect(@html).to have_selector(xpath, text: 2)
-    end
-
-    it '次のページへのボタンが表示されていること' do
-      xpath = "#{paging_xpath}/li[@class='page-item']/span[@class='next']" \
-              '/a[href="/predictions?page=2"]'
-      expect(@html).to have_selector(xpath, text: I18n.t('views.pagination.next'))
-    end
-
-    it '最後のページへのボタンが表示されていること' do
-      xpath = "#{paging_xpath}/li[@class='page-item']/span[@class='last']/a"
-      expect(@html).to have_selector(xpath, text: I18n.t('views.pagination.last'))
-    end
-
-    %w[実行開始日時 モデル 期間 結果].each do |header|
-      it "ヘッダー(#{header})があること" do
-        xpath = "#{base_xpath}/table[@class='table table-hover']/thead/th"
-        expect(@html).to have_selector(xpath, text: header)
+    %w[実行開始日時 モデル 期間 結果].each_with_index do |text, i|
+      it "#{i + 1}列目のヘッダーが#{text}であること" do
+        is_asserted_by { @table.xpath('//thead/th')[i].text == text }
       end
     end
 
-    it 'データの数が正しいこと' do
-      xpath = "#{base_xpath}/table[@class='table table-hover']/tbody/tr"
-      expect(@html).to have_xpath(xpath, count: expected[:size])
+    it 'ジョブの数が正しいこと' do
+      is_asserted_by { @table.xpath('//tbody/tr').size == rows }
+    end
+  end
+
+  shared_examples 'ジョブの状態が正しいこと' do |state: nil, result: nil|
+    it 'アイコンが正しいこと' do
+      rows =
+        @html.xpath("#{table_panel_xpath}/table[@class='table table-hover']/tbody/tr")
+
+      rows.each do |row|
+        glyphicon_name = result ? icon_class[state][result] : icon_class[state]
+        span_class = "glyphicon glyphicon-#{glyphicon_name}"
+        icon = row.children.search('td')[3].children
+                  .search("span[@class='#{span_class}']")
+        is_asserted_by { icon.present? }
+      end
     end
 
-    it '背景色と結果が正しいこと', if: expected[:size] > 0 do
-      row = @html.gsub("\n", '').scan(/<tr.*?tr>/).first
+    it 'アイコンの色が正しいこと' do
+      rows =
+        @html.xpath("#{table_panel_xpath}/table[@class='table table-hover']/tbody/tr")
 
-      is_asserted_by do
-        row.match(/warning.*question-sign/) or
-          row.match(/success.*arrow-(up|down|right)/)
+      rows.each do |row|
+        glyphicon_color = result ? icon_color[state][result] : icon_color[state]
+        color = row.children.search('td')[3].children.search('span').attribute('style')
+        is_asserted_by { color.value == "color: #{glyphicon_color}" }
       end
     end
   end
@@ -122,15 +117,66 @@ describe 'predictions/manage', type: :view do
 
   before(:each) do
     render template: 'predictions/manage', layout: 'layouts/application'
-    @html ||= response
+    @html ||= Nokogiri.parse(response)
   end
 
-  describe '<html><body>' do
+  context '実行中の場合' do
     include_context 'トランザクション作成'
-    include_context '予測ジョブを登録する', 10
-    it_behaves_like 'ヘッダーが表示されていること'
-    it_behaves_like '入力フォームが表示されていること'
-    it_behaves_like 'ジョブ実行履歴が表示されていること',
-                    expected: {size: 1, total: 10, from: 1, to: 1}
+    include_context '予測ジョブを登録する'
+    include_context 'HTML初期化'
+    it_behaves_like '画面共通テスト'
+    it_behaves_like 'ページングボタンが表示されていないこと'
+    it_behaves_like 'ジョブの状態が正しいこと', state: 'processing'
+  end
+
+  context '完了している場合' do
+    context '予測結果が上昇の場合' do
+      param = {state: 'completed', result: 'up'}
+      include_context 'トランザクション作成'
+      include_context '予測ジョブを登録する', attribute: default_attribute.merge(param)
+      include_context 'HTML初期化'
+      it_behaves_like '画面共通テスト'
+      it_behaves_like 'ページングボタンが表示されていないこと'
+      it_behaves_like 'ジョブの状態が正しいこと', param
+    end
+
+    context '予測結果が下降の場合' do
+      param = {state: 'completed', result: 'down'}
+      include_context 'トランザクション作成'
+      include_context '予測ジョブを登録する', attribute: default_attribute.merge(param)
+      include_context 'HTML初期化'
+      it_behaves_like '画面共通テスト'
+      it_behaves_like 'ページングボタンが表示されていないこと'
+      it_behaves_like 'ジョブの状態が正しいこと', param
+    end
+
+    context '予測結果がレンジの場合' do
+      param = {state: 'completed', result: 'range'}
+      include_context 'トランザクション作成'
+      include_context '予測ジョブを登録する', attribute: default_attribute.merge(param)
+      include_context 'HTML初期化'
+      it_behaves_like '画面共通テスト'
+      it_behaves_like 'ページングボタンが表示されていないこと'
+      it_behaves_like 'ジョブの状態が正しいこと', param
+    end
+  end
+
+  context 'エラーの場合' do
+    param = {state: 'error'}
+    include_context 'トランザクション作成'
+    include_context '予測ジョブを登録する', attribute: default_attribute.merge(param)
+    include_context 'HTML初期化'
+    it_behaves_like '画面共通テスト'
+    it_behaves_like 'ページングボタンが表示されていないこと'
+    it_behaves_like 'ジョブの状態が正しいこと', state: 'error'
+  end
+
+  context "予測ジョブ情報が#{per_page * (Kaminari.config.window + 2)}件の場合" do
+    total = per_page * (Kaminari.config.window + 2)
+    include_context 'トランザクション作成'
+    include_context '予測ジョブを登録する', total: total
+    include_context 'HTML初期化'
+    it_behaves_like '画面共通テスト', expected: {total: total}
+    it_behaves_like 'ページングボタンが表示されていること', model: 'prediction'
   end
 end
