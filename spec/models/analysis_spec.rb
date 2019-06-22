4b192abc8cc052fbf5898fc27ea11f6df99a058c
Leonis0813 <Leonis.0813@gmail.com>
2019-06-22 10:19:58 +0000

    update analysis model spec

--- spec/models/analysis_spec.rb
+++ spec/models/analysis_spec.rb
@@ -3,24 +3,9 @@
 require 'rails_helper'
 
 describe Analysis, type: :model do
-  shared_context 'Analysisオブジェクトを検証する' do |params|
-    before(:all) do
-      @analysis = Analysis.new(params)
-      @analysis.validate
-    end
-  end
-
-  shared_examples '検証結果が正しいこと' do |result|
-    it_is_asserted_by { @analysis.errors.empty? == result }
-  end
-
-  shared_examples 'エラーメッセージが正しいこと' do |expected_messages|
-    it_is_asserted_by { @analysis.errors.messages == expected_messages }
-  end
-
   describe '#validates' do
     describe '正常系' do
-      valid_params = {
+      valid_attribute = {
         from: [
           '1000-01-01',
           '1000/01/01',
@@ -45,62 +30,42 @@ describe Analysis, type: :model do
           '01/01/1001 00:00:00',
           '10010101 00:00:00',
         ],
+        pair: %w[AUDJPY CADJPY CHFJPY EURJPY EURUSD GBPJPY NZDJPY USDJPY],
         batch_size: 100,
         state: %w[processing completed error],
       }
 
-      test_cases = CommonHelper.generate_test_case(valid_params).select do |test_case|
-        test_case.keys == valid_params.keys
-      end
-
-      test_cases.each do |params|
-        context "フォームに#{params.keys.join(',')}を指定した場合" do
-          include_context 'Analysisオブジェクトを検証する', params
-          it_behaves_like '検証結果が正しいこと', true
-        end
-      end
+      it_behaves_like '正常な値を指定した場合のテスト', valid_attribute
     end
 
     describe '異常系' do
-      valid_params = {
-        from: '1000-01-01',
-        to: '1001-01-01',
-        batch_size: 100,
-        state: 'processing',
+      invalid_attribute = {
+        from: ['invalid', 0, 0.0, nil],
+        to: ['invalid', 0, 0.0, nil],
+        pair: ['invalid', 0, 0.0, nil],
+        batch_size: ['invalid', 0, 1.0, nil],
+        state: ['invalid', 0, 0.0, nil],
       }
-      invalid_params = {
-        from: [nil],
-        to: [nil],
-        batch_size: [-1, 0],
-        state: [nil, 'invalid'],
-      }
-
-      CommonHelper.generate_test_case(invalid_params).each do |params|
-        context "フォームに#{params.keys.join(',')}を指定した場合" do
-          error_messages = params.map {|key, _| [key, ['invalid']] }.to_h
-          include_context 'Analysisオブジェクトを検証する', valid_params.merge(params)
-          it_behaves_like '検証結果が正しいこと', false
-          it_behaves_like 'エラーメッセージが正しいこと', error_messages
-        end
-      end
-
       invalid_period = {
         from: %w[1000-01-02 1000/01/02 02-01-1000 02/01/1000 10000102],
         to: %w[1000-01-01 1000/01/01 01-01-1000 01/01/1000 10000101],
       }
+      absent_keys = invalid_attribute.keys - %i[from to pair]
 
-      test_cases = CommonHelper.generate_test_case(invalid_params.merge(invalid_period))
-      test_cases.select! do |test_case|
-        test_case.include?(:from) and test_case.include?(:to)
-      end
+      it_behaves_like '必須パラメーターがない場合のテスト', absent_keys
+      it_behaves_like '不正な値を指定した場合のテスト', invalid_attribute
 
-      test_cases.each do |params|
+      CommonHelper.generate_test_case(invalid_period).each do |attribute|
         context '期間が不正な場合' do
-          error_messages = params.map {|key, _| [key, ['invalid']] }.to_h
-          include_context 'Analysisオブジェクトを検証する', valid_params.merge(params)
+          before (:all) do
+            @analysis = build(:analysis, attribute)
+            @analysis.validate
+          end
 
-          it_behaves_like '検証結果が正しいこと', false
-          it_behaves_like 'エラーメッセージが正しいこと', error_messages
+          it 'fromとtoが不正なこと' do
+            is_asserted_by { @analysis.errors.messages[:from].include?('invalid') }
+            is_asserted_by { @analysis.errors.messages[:to].include?('invalid') }
+          end
         end
       end
     end
--- spec/support/helper/common_helper.rb
+++ spec/support/helper/common_helper.rb
@@ -9,28 +9,24 @@ module CommonHelper
 
   def generate_test_case(params)
     [].tap do |test_cases|
-      params.keys.size.times do |i|
-        params.keys.combination(i + 1).each do |some_keys|
-          tmp_test_cases = [].tap do |tests|
-            Array.wrap(params[some_keys.first]).each do |value|
-              tests << {some_keys.first => value}
-            end
-
-            some_keys[1..-1].each do |key|
-              tmp_tests = [].tap do |tmp_test|
-                tests.each do |test|
-                  Array.wrap(params[key]).each do |value|
-                    tmp_test << test.merge(key => value)
-                  end
-                end
-              end
-              tests = tmp_tests
-            end
-            break tests
-          end
-          test_cases << tmp_test_cases
+      tmp_test_cases = [].tap do |tests|
+        Array.wrap(params[params.keys.first]).each do |value|
+          tests << {params.keys.first => value}
         end
+
+        params.keys[1..-1].each do |key|
+          tmp_tests = [].tap do |tmp_test|
+            tests.each do |test|
+              Array.wrap(params[key]).each do |value|
+                tmp_test << test.merge(key => value)
+              end
+            end
+          end
+          tests = tmp_tests
+        end
+        break tests
       end
+      test_cases << tmp_test_cases
     end.flatten
   end
 
