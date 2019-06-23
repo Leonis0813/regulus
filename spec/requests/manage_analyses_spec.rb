fe166e148fb6ff38eecc8894fe56784fc4b48153
Leonis0813 <Leonis.0813@gmail.com>
2019-06-23 02:06:36 +0000

    add execution test

--- spec/requests/manage_analyses_spec.rb
+++ spec/requests/manage_analyses_spec.rb
@@ -36,4 +36,27 @@ describe 'ブラウザで分析する', type: :request do
       is_asserted_by { @driver.find_element(:xpath, xpath).text == text }
     end
   end
+
+  describe '分析を実行する' do
+    before(:all) do
+      @driver.get("#{base_url}/analyses")
+      @driver.find_element(:id, 'analysis_from').send_keys('2000-01-01 00:00:00')
+      @driver.find_element(:id, 'analysis_to').send_keys('2000-01-10 23:59:59')
+      @driver.find_element(:id, 'analysis_batch_size').send_keys(100)
+      @driver.find_element(:xpath, '//form/input[@value="実行"]').click
+      @wait.until { @driver.find_element(:class, 'modal-body').displayed? }
+    end
+
+    it 'タイトルが正しいこと' do
+      xpath = '//div[@class="modal-header"]/h4[@class="modal-title"]'
+      text = '分析を開始しました'
+      is_asserted_by { @driver.find_element(:xpath, xpath).text == text }
+    end
+
+    it 'メッセージが正しいこと' do
+      xpath = '//div[@class="modal-body"]/div'
+      text = '終了後、メールにて結果を通知します'
+      is_asserted_by { @driver.find_element(:xpath, xpath).text == text }
+    end
+  end
 end
