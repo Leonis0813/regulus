5ca60ff6d4aa18159221c59e513ac8bd7172a078
Leonis0813 <Leonis.0813@gmail.com>
2021-01-05 15:33:43 +0000

    fix bug

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,8 +1,9 @@
 import mysql.connector as mysql
 import numpy as np
-import tensorflow as tf
 import os
+import pandas as pd
 import sys
+import tensorflow as tf
 import yaml
 
 args = sys.argv
@@ -39,47 +40,54 @@ min = min(
   raw_data['ma75'].min(),
   raw_data['ma200'].min()
 )
-for column in raw_data.columns:
+for column in list(set(raw_data.columns) - set(['time'])):
   normalized_data[column] = 2.0 * (raw_data[column] - min) / (max - min) - 1.0
 
 raw_data.to_csv(WORKDIR + '/tmp/raw_data.csv', index=False)
 normalized_data.to_csv(WORKDIR + '/tmp/normalized_data.csv', index=False)
 
-test_data = normalized_data.to_numpy.ravel()
+test_data = []
+for index in range(0, 20):
+  test_data.extend([
+    normalized_data['open'][index],
+    normalized_data['ma25'][index],
+    normalized_data['ma75'][index],
+    normalized_data['ma200'][index],
+  ])
 
-x = tf.placeholder(tf.float32, [None, 720])
+x = tf.placeholder(tf.float32, [None, 80])
 
-w_1 = tf.Variable(tf.truncated_normal([720, 512], stddev=0.1), name="w1")
-b_1 = tf.Variable(tf.zeros([512]), name="b1")
+w_1 = tf.Variable(tf.truncated_normal([80, 64], stddev=0.1), name="w1")
+b_1 = tf.Variable(tf.zeros([64]), name="b1")
 h_1 = tf.nn.relu(tf.matmul(x, w_1) + b_1)
 
-w_2 = tf.Variable(tf.truncated_normal([512, 128], stddev=0.1), name="w2")
-b_2 = tf.Variable(tf.zeros([128]), name="b2")
+w_2 = tf.Variable(tf.truncated_normal([64, 32], stddev=0.1), name="w2")
+b_2 = tf.Variable(tf.zeros([32]), name="b2")
 h_2 = tf.nn.relu(tf.matmul(h_1, w_2) + b_2)
 
-w_3 = tf.Variable(tf.truncated_normal([128, 32], stddev=0.1), name="w3")
-b_3 = tf.Variable(tf.zeros([32]), name="b3")
+w_3 = tf.Variable(tf.truncated_normal([32, 16], stddev=0.1), name="w3")
+b_3 = tf.Variable(tf.zeros([16]), name="b3")
 h_3 = tf.nn.relu(tf.matmul(h_2, w_3) + b_3)
 
-w_4 = tf.Variable(tf.truncated_normal([32, 8], stddev=0.1), name="w4")
+w_4 = tf.Variable(tf.truncated_normal([16, 8], stddev=0.1), name="w4")
 b_4 = tf.Variable(tf.zeros([8]), name="b4")
 h_4 = tf.nn.relu(tf.matmul(h_3, w_4) + b_4)
 
-w_5 = tf.Variable(tf.truncated_normal([8, 1], stddev=0.1), name="w5")
-b_5 = tf.Variable(tf.zeros([1]), name="b5")
+w_5 = tf.Variable(tf.truncated_normal([8, 2], stddev=0.1), name="w5")
+b_5 = tf.Variable(tf.zeros([2]), name="b5")
 out = tf.nn.softmax(tf.matmul(h_4, w_5) + b_5)
 
 saver = tf.train.Saver()
 
 with tf.Session() as sess:
   saver.restore(sess, os.path.dirname(os.path.abspath(args[0])) + '/tmp/model.ckpt')
-  result = sess.run(out, feed_dict={x:test_data})
+  result = sess.run(out, feed_dict={x:[test_data]})
 
   result_file.write("from: " + records[-1]['time'].strftime('%Y-%m-%d %H:%M:%S') + "\n")
   result_file.write("to: " + records[0]['time'].strftime('%Y-%m-%d %H:%M:%S') + "\n")
 
-  prediction = result[0][0]
-  if prediction > 0.5:
+  up, down = result[0]
+  if up > down:
     result_file.write("result: up\n")
   else:
     result_file.write("result: down\n")
