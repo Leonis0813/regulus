7b6a1814465e8e15dc9c9c6978a07b5a280add47
Leonis0813 <Leonis.0813@gmail.com>
2021-01-08 03:56:23 +0000

    wait latest data if production

--- app/jobs/prediction_job.rb
+++ app/jobs/prediction_job.rb
@@ -5,6 +5,8 @@ class PredictionJob < ApplicationJob
 
   queue_as :regulus
 
+  MAX_RETRY = 60
+
   def perform(prediction_id, model_dir)
     prediction = Prediction.find(prediction_id)
     tmp_dir = Rails.root.join('scripts', 'tmp')
@@ -13,8 +15,10 @@ class PredictionJob < ApplicationJob
     FileUtils.mkdir_p(tmp_dir)
     unzip_model(File.join(model_dir, prediction.model), tmp_dir)
 
-    prediction.update!(pair: YAML.load_file(File.join(tmp_dir, 'metadata.yml'))['pair'])
+    pair = YAML.load_file(File.join(tmp_dir, 'metadata.yml'))['pair']
+    prediction.update!(pair: pair)
 
+    polling_zosma(pair) if Settings.prediction.wait_latest
     execute_script('predict.py')
 
     FileUtils.mv(File.join(tmp_dir, 'result.yml'), model_dir)
@@ -26,5 +30,34 @@ class PredictionJob < ApplicationJob
     Rails.logger.error(e.message)
     Rails.logger.error(e.backtrace.join("\n"))
     prediction.update!(state: Prediction::STATE_ERROR)
+  end
+
+  private
+
+  def polling_zosma(pair)
+    found = false
+
+    MAX_RETRY.times do
+      found = Zosma::CandleStick.exists?(
+        from: 1.day.ago.strftime('%F 00:00:00'),
+        to: 1.day.ago.strftime('%F 23:59:59'),
+        pair: pair
+        time_frame: 'D1',
+      )
+      break if found
+      sleep(60)
+    end
+    raise StandardError unless found
+
+    MAX_RETRY.times do
+      found = Zosma::MovingAverage.exists?(
+        time: 1.day.ago.strftime('%F 00:00:00'),
+        pair: pair
+        time_frame: 'D1',
+      )
+      break if found
+      sleep(60)
+    end
+    raise StandardError unless found
   end
 end
