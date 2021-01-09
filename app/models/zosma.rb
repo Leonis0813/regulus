a76eedd234f3c1cef039d89fbc1c0f0dd23c7447
Leonis0813 <Leonis.0813@gmail.com>
2021-01-09 12:57:22 +0000

    fix style

--- app/models/zosma.rb
+++ app/models/zosma.rb
@@ -1,6 +1,6 @@
 module Zosma
   class Base < ApplicationRecord
-    file_path = Rails.root.join('config', 'zosma', 'database.yml')
+    file_path = Rails.root.join('config/zosma/database.yml')
     establish_connection(YAML.load_file(file_path)[Rails.env])
     self.abstract_class = true
   end
