e797e28075d0d211997a3850e3156e05bd8f28b2
Leonis0813 <Leonis.0813@gmail.com>
2021-04-01 15:08:51 +0000

    add action cable settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+
   get '/analyses' => 'analyses#manage'
   post '/analyses' => 'analyses#execute'
   put '/analyses/result' => 'analyses#upload_result'
