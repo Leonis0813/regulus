ceec4684ab116e32b5b856bb04d11564f474c362
Leonis0813 <Leonis.0813@gmail.com>
2021-04-01 11:41:05 +0000

    add performed_at and new state

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210307080243) do
+ActiveRecord::Schema.define(version: 20210401113804) do
 
   create_table "analyses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "analysis_id"
-    t.datetime "from",                   default: '1970-01-01 00:00:00', null: false
-    t.datetime "to",                     default: '2286-11-20 17:46:40', null: false
-    t.string   "pair",                   default: "USDJPY",              null: false
-    t.integer  "batch_size",             default: 0,                     null: false
-    t.float    "min",         limit: 24
-    t.float    "max",         limit: 24
-    t.string   "state",                                                  null: false
-    t.datetime "created_at",                                             null: false
-    t.datetime "updated_at",                                             null: false
+    t.datetime "from",                    default: '1970-01-01 00:00:00', null: false
+    t.datetime "to",                      default: '2286-11-20 17:46:40', null: false
+    t.string   "pair",                    default: "USDJPY",              null: false
+    t.integer  "batch_size",              default: 0,                     null: false
+    t.float    "min",          limit: 24
+    t.float    "max",          limit: 24
+    t.string   "state",                                                   null: false
+    t.datetime "performed_at"
+    t.datetime "created_at",                                              null: false
+    t.datetime "updated_at",                                              null: false
     t.index ["analysis_id"], name: "index_analyses_on_analysis_id", unique: true, using: :btree
   end
 
@@ -36,6 +37,7 @@ ActiveRecord::Schema.define(version: 20210307080243) do
     t.string   "means",         default: "manual", null: false
     t.string   "result"
     t.string   "state",                            null: false
+    t.datetime "performed_at"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.index ["analysis_id"], name: "index_predictions_on_analysis_id", using: :btree
