98018430073be58c77bf1579bf11443818550de7
Leonis0813 <Leonis.0813@gmail.com>
2021-06-15 14:13:48 +0000

    create evaluations controller

---
+++ app/controllers/evaluations_controller.rb
@@ -0,0 +1,48 @@
+class EvaluationsController < ApplicationController
+  before_action :check_request_evaluation, only: %i[show]
+
+  def index
+    @evaluation = Evaluation.new
+    @evaluations = Evaluation.all.order(created_at: :desc).page(params[:page])
+  end
+
+  def execute
+    check_absent_params(%i[model from to], execute_params)
+
+    model = execute_params[:model]
+    raise BadRequest, 'invalid_param_model' unless model.respond_to?(:original_filename)
+
+    evaluation = Evaluation.new(
+      model: model.original_filename,
+      from: execute_params[:from],
+      to: execute_params[:to],
+    )
+
+    unless evaluation.save
+      error_codes = evaluation.errors.messages.keys.sort.map do |key|
+        "invalid_param_#{key}"
+      end
+      raise BadRequest, error_codes
+    end
+
+    EvaluationJob.perform_later(evaluation)
+    render status: :ok, json: {}
+  end
+
+  def show
+  end
+
+  private
+
+  def check_request_evaluation
+    raise NotFound unless evaluation
+  end
+
+  def evaluation
+    @evaluation ||= Evaluation.find_by(request.path_parameters.slice(:evaluation_id))
+  end
+
+  def execute_params
+    @execute_params ||= request.request_parameters.slice(:model, :from, :to)
+  end
+end
